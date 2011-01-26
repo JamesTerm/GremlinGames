e345d72654127700a827310f5c1c693ec69a3e45
james <james@e2c3bcc0-b32a-0410-840c-db224dcf21cb>
2011-01-26 14:04:13 +0000

    Added Robot_Tank (not yet implemented)

---
+++ Source/James/AI_Tester/AI_Tester/Robot_Tank.cpp
@@ -0,0 +1,30 @@
+#include "stdafx.h"
+#include "AI_Tester.h"
+
+using namespace AI_Tester;
+using namespace GG_Framework::Base;
+
+Robot_Tank::Robot_Tank(const char EntityName[]) : Ship_2D(EntityName), m_LeftLinearVelocity(0.0),m_RightLinearVelocity(0.0)
+{
+}
+
+void Robot_Tank::ApplyThrusters(PhysicsEntity_2D &PhysicsToUse,const osg::Vec2d &LocalForce,double LocalTorque,double TorqueRestraint,double dTime_s)
+{
+	class Internal
+	{
+		public:
+			void UpdateVelocities()
+			{
+
+			}
+			void InterpolateThrusterChanges(osg::Vec2d &LocalForce,double LocalTorque)
+			{
+			}
+	} _;
+	_.UpdateVelocities();
+	osg::Vec2d NewLocalForce(LocalForce);
+	double NewTorque=LocalTorque;
+	_.InterpolateThrusterChanges(NewLocalForce,NewTorque);
+	__super::ApplyThrusters(PhysicsToUse,NewLocalForce,NewTorque,TorqueRestraint,dTime_s);
+}
+
