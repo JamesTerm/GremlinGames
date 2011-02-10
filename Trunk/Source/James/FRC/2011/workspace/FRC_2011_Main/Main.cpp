1f0acc20cbe1e73f2e544fecc2a14774ef91e239
james <james@e2c3bcc0-b32a-0410-840c-db224dcf21cb>
2011-02-10 05:57:12 +0000

    Added Autonomous implementation to advance robot 5 meters.

--- Trunk/Source/James/FRC/2011/workspace/FRC_2011_Main/Main.cpp
+++ Trunk/Source/James/FRC/2011/workspace/FRC_2011_Main/Main.cpp
@@ -22,6 +22,113 @@ const bool c_UseDefaultControls=false;
 
 
 
+class SetUp_Manager
+{
+	protected:
+		//Note: The order of the members are critical, as they are instantiated in the constructor
+		Driver_Station_Joystick m_Joystick;  
+		Framework::UI::JoyStick_Binder m_JoyBinder;
+		Ship_Properties m_RobotProps;  //This will be upgraded soon
+		Robot_Control m_Control; // robot drive system
+		FRC_2011_Robot *m_pRobot; //This is a scoped pointer with late binding
+		Framework::Base::EventMap m_EventMap;
+		UI_Controller *m_pUI;
+	public:
+		SetUp_Manager() : m_Joystick(1,0), //for now 1 joystick starting at port 0 (i.e. no offset)
+			m_JoyBinder(m_Joystick),m_pRobot(NULL),m_pUI(NULL)
+		{
+			m_Control.Initialize(&m_RobotProps);
+			m_pRobot = new FRC_2011_Robot("FRC2011_Robot",&m_Control);
+			m_pRobot->Initialize(m_EventMap,&m_RobotProps);
+			//Bind the ship's eventmap to the joystick
+			m_JoyBinder.SetControlledEventMap(m_pRobot->GetEventMap());
+
+			//To to bind the UI controller to the robot
+			AI_Base_Controller *controller=m_pRobot->GetController();
+			assert(controller);
+			m_pUI=new UI_Controller(m_JoyBinder,controller); 
+			if (controller->Try_SetUIController(m_pUI))
+			{
+				//Success... now to let the entity set things up
+				m_pUI->HookUpUI(true);
+			}
+			else
+			{
+				m_pUI->Set_AI_Base_Controller(NULL);   //no luck... flush ship association
+				assert(false);
+			}
+		}
+		void TimeChange(double dTime_s)
+		{
+			if (dTime_s==0.0) return; //avoid division by zero errors
+			m_JoyBinder.UpdateJoyStick(dTime_s);
+			m_pRobot->TimeChange(dTime_s);
+		}
+
+		~SetUp_Manager()
+		{
+			//Note: in visual studio the delete pointer implicitly checks for NULL, but I do not want to assume this for wind river.
+			if (m_pUI)
+			{
+				delete m_pUI;
+				m_pUI=NULL;
+			}
+			if (m_pRobot)
+			{
+				delete m_pRobot;
+				m_pRobot=NULL;
+			}
+		}
+};
+
+class SetUp_Autonomous : public SetUp_Manager
+{
+	private:
+		void StopLoop()
+		{
+			m_StillRunning=false;
+		}
+		bool m_StillRunning;
+		IEvent::HandlerList ehl;
+	public:
+		SetUp_Autonomous()
+		{
+			m_pUI->SetAutoPilot(true);  //we are not driving the robot
+			//Now to set up our goal
+			Ship_Tester *ship=m_pRobot;  //we can always cast down
+			double x=0.0;
+			double y=5.0;  //five meters
+			assert(ship);
+			{
+				Goal *oldgoal=ship->ClearGoal();
+				if (oldgoal)
+					delete oldgoal;
+				//Construct a way point
+				WayPoint wp;
+				wp.Position[0]=x;
+				wp.Position[1]=y;
+				wp.Power=1.0;
+				//Now to setup the goal
+				Goal_Ship_MoveToPosition *goal=new Goal_Ship_MoveToPosition(ship->GetController(),wp);
+
+				//wrap the goal in a notify goal
+				Goal_NotifyWhenComplete *notify_goal=new Goal_NotifyWhenComplete(m_EventMap,"Complete"); //will fire Complete once it is done
+				notify_goal->AddSubgoal(goal);  //only one goal (for now)
+				notify_goal->Activate(); //now with the goal(s) loaded activate it
+				//Now to subscribe to this event... it will call Stop Loop when the goal is finished
+				m_EventMap.Event_Map["Complete"].Subscribe(ehl,*this,&SetUp_Autonomous::StopLoop);
+				ship->SetGoal(notify_goal);
+			}
+
+		}
+		bool IsStillRunning()
+		{
+			return m_StillRunning;
+		}
+};
+
+
+
 
 //This is the main robot class used for FRC 2011 
 //The SimpleRobot class is the base of a robot application that will automatically call your
@@ -33,13 +140,20 @@ public:
 	//Drive left & right motors for 2 seconds then stop
 	void Autonomous(void)
 	{
-		RobotDrive myRobot(1,2,3,4); // robot drive system
-		myRobot.SetExpiration(0.1);
-		
-		myRobot.SetSafetyEnabled(false);
-		myRobot.Drive(0.5, 0.0); 	// drive forwards half speed
-		Wait(2.0); 				//    for 2 seconds
-		myRobot.Drive(0.0, 0.0); 	// stop robot
+		SetUp_Autonomous main_autonomous;
+		double tm = GetTime();
+		while (main_autonomous.IsStillRunning())
+		{
+			double time=GetTime() - tm;
+			tm=GetTime();
+			//Framework::Base::DebugOutput("%f\n",time),
+			//I'll keep this around as a synthetic time option for debug purposes
+			//time=0.020;
+			main_autonomous.TimeChange(time);
+			//TODO see how fast the loop runs (if possible)
+			Wait(0.005);				
+		}
+
 	}
 	void OperatorControl(void)
 	{
@@ -59,48 +173,20 @@ public:
 		}
 		else
 		{
-			//TODO all of this will be moved to a manager class when starting the autonomous implementation
-			
-			//Set up input
-			Driver_Station_Joystick joystick(1,0);  //for now 1 joystick starting at port 0 (i.e. no offset)
-			Framework::UI::JoyStick_Binder joy_binder(joystick);
-			
-			Ship_Properties robot_props;  //This will be upgraded soon
-			Robot_Control control; // robot drive system
-			control.Initialize(&robot_props);
-			FRC_2011_Robot robot("FRC2011_Robot",&control);
-			Framework::Base::EventMap eventMap;
-			robot.Initialize(eventMap,&robot_props);
-			//Bind the ship's eventmap to the joystick
-			joy_binder.SetControlledEventMap(robot.GetEventMap());
-			//To to bind the UI controller to the robot
-			AI_Base_Controller *controller=robot.GetController();
-			assert(controller);
-			UI_Controller UI(joy_binder,controller); 
-			
-			if (controller->Try_SetUIController(&UI))
-			{
-				//Success... now to let the entity set things up
-				UI.HookUpUI(true);
-			}
-			else
-			{
-				UI.Set_AI_Base_Controller(NULL);   //no luck... flush ship association
-				assert(false);
-			}
-			
+			SetUp_Manager main;
 			double tm = GetTime();
 			while (IsOperatorControl())
 			{
 				//I'll keep this around as a synthetic time option for debug purposes
-				//double time=0.016;
+				//double time=0.020;
 				double time=GetTime() - tm;
 				tm=GetTime();
-				joy_binder.UpdateJoyStick(time);
-				robot.TimeChange(time);
-				//It seems this will yield about a 20 ms loop, so we'll keep it as it is in the example
+				//Framework::Base::DebugOutput("%f\n",time),
+				main.TimeChange(time);
+				//TODO see how fast the loop runs (if possible)
 				Wait(0.005);				
 			}
+
 		}
 	}
 };
