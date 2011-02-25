0e2c16085951b42adea4c221cd2724d68f772764
james <james@e2c3bcc0-b32a-0410-840c-db224dcf21cb>
2011-02-25 12:11:17 +0000

    corrected the ordering of the simulation.  Now it should be accurate (except it currently is not using potentiometer ratios).

--- Trunk/Source/James/AI_Tester/AI_Tester/Calibration_Testing.h
+++ Trunk/Source/James/AI_Tester/AI_Tester/Calibration_Testing.h
@@ -6,9 +6,12 @@ class Potentiometer_Tester : public Ship_1D
 		Potentiometer_Tester();
 		void UpdatePotentiometerVelocity(double Velocity);
 		virtual double GetPotentiometerCurrentPosition();
-		virtual void TimeChange(double dTime_s);
+		//This is broken up so that the real interface does not have to pass time
+		void SetTimeDelta(double dTime_s) {m_Time_s=dTime_s;}
+		void TimeChange();
 	protected:
 	private:
+		double m_Time_s;
 		Ship_1D_Properties m_PotentiometerProps;
 		GG_Framework::Base::EventMap m_DummyMap;
 };
