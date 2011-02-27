64d1b9819481900f902df1c31978ffaaa695972b
james <james@e2c3bcc0-b32a-0410-840c-db224dcf21cb>
2011-02-27 01:33:13 +0000

    Code cleanup: removed comments as they no longer apply

--- Trunk/Source/James/AI_Tester/AI_Tester/FRC2011_Robot.h
+++ Trunk/Source/James/AI_Tester/AI_Tester/FRC2011_Robot.h
@@ -12,8 +12,8 @@ class Robot_Control_Interface
 		virtual void Initialize(const Entity_Properties *props)=0;
 		//Encoders populate this with current velocity of motors
 		virtual void GetLeftRightVelocity(double &LeftVelocity,double &RightVelocity)=0;  ///< in meters per second
-		virtual void UpdateLeftRightVoltage(double LeftVoltage,double RightVoltage)=0;  ///< in meters per second
-		virtual void UpdateArmVoltage(double Voltage)=0;  ///< in meters per second
+		virtual void UpdateLeftRightVoltage(double LeftVoltage,double RightVoltage)=0;
+		virtual void UpdateArmVoltage(double Voltage)=0;
 		///This is a implemented by reading the potentiometer and converting its value to correspond to the arm's current angle
 		///This is in radians of the arm's gear ratio
 		virtual double GetArmCurrentPosition()=0;
