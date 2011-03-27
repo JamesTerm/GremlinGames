86963e14fb5b14be19e838e1a1cf0cfa538b1b93
james <james@e2c3bcc0-b32a-0410-840c-db224dcf21cb>
2011-03-27 21:41:43 +0000

    Applied PID controls to testing harness code

--- Trunk/Source/James/FRC/2011/DriverStationTest/FRC2011_Robot.h
+++ Trunk/Source/James/FRC/2011/DriverStationTest/FRC2011_Robot.h
@@ -39,6 +39,7 @@ class FRC_2011_Robot : public Robot_Tank
 				void SetPos9feet();
 				Robot_Control_Interface * const m_RobotControl;
 				double m_LastNormalizedVelocity;  //this is managed direct from being set to avoid need for precision tolerance
+				PIDController2 m_PIDController;
 				double m_LastPosition;  //used for calibration
 				double m_CalibratedScaler; //used for calibration
 				double m_LastTime; //used for calibration
@@ -58,6 +59,7 @@ class FRC_2011_Robot : public Robot_Tank
 		//typedef  Robot_Tank __super;
 		Robot_Control_Interface * const m_RobotControl;
 		Robot_Arm m_Arm;
+		PIDController2 m_PIDController;
 		double m_CalibratedScaler; //used for calibration
 		bool m_UsingEncoders;
 };
