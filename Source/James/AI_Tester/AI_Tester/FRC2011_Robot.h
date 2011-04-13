859ed8215950e6ab2f220985ae85aa325480153d
james <james@e2c3bcc0-b32a-0410-840c-db224dcf21cb>
2011-04-13 16:35:05 +0000

    Now using two PID controllers for the encoders.  This method proves to be more stable on turns but still needs work on applying the straightening... (and needs to remain stable during a turn).

--- Source/James/AI_Tester/AI_Tester/FRC2011_Robot.h
+++ Source/James/AI_Tester/AI_Tester/FRC2011_Robot.h
@@ -81,7 +81,7 @@ class FRC_2011_Robot : public Robot_Tank
 		//typedef  Robot_Tank __super;
 		Robot_Control_Interface * const m_RobotControl;
 		Robot_Arm m_Arm;
-		PIDController2 m_PIDController;
+		PIDController2 m_PIDController_Left,m_PIDController_Right;
 		double m_CalibratedScaler; //used for calibration
 		bool m_UsingEncoders;
 };
