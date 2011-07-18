3ee096778c6a9dfa341085b53cfe75e3d8e4b827
james <james@e2c3bcc0-b32a-0410-840c-db224dcf21cb>
2011-07-18 22:17:32 +0000

    re-enabled the Arm Claw

--- Source/James/FRC/2011/workspace/FRC_2011/UI_Controller.cpp
+++ Source/James/FRC/2011/workspace/FRC_2011/UI_Controller.cpp
@@ -74,7 +74,7 @@ UI_Controller::UI_Controller(JoyStick_Binder &joy,AI_Base_Controller *base_contr
 	joy.AddJoy_Button_Default( 6,"Arm_SetPos3feet",false,false,"Joystick_2");
 	joy.AddJoy_Button_Default(10,"Arm_SetPos6feet",false,false,"Joystick_2");
 	joy.AddJoy_Button_Default( 9,"Arm_SetPos9feet",false,false,"Joystick_2");
-	//joy.AddJoy_Button_Default( 0,"Arm_Claw",true,false,"Joystick_2");
+	joy.AddJoy_Button_Default( 0,"Arm_Claw",true,false,"Joystick_2");
 
 	joy.AddJoy_Button_Default( 7,"Robot_CloseDoor",true,false,"Joystick_2");
 	
