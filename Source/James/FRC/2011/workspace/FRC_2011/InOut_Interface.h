6af9cae30af693e0893d5a1ff8e373fd656c7611
james <james@e2c3bcc0-b32a-0410-840c-db224dcf21cb>
2011-10-25 14:51:05 +0000

    Used a macro to identify work-around and made the busted encoders use a dummy digital input to avoid writing/using other digital inputs.

--- Source/James/FRC/2011/workspace/FRC_2011/InOut_Interface.h
+++ Source/James/FRC/2011/workspace/FRC_2011/InOut_Interface.h
@@ -1,6 +1,8 @@
 #ifndef INOUT_INTERFACE_H_
 #define INOUT_INTERFACE_H_
 
+#define __UseEncoderWorkaround__
+
 class Robot_Control : public Robot_Control_Interface
 {
 	public:
@@ -29,9 +31,13 @@ class Robot_Control : public Robot_Control_Interface
 		Solenoid m_OnClaw,m_OffClaw;
 		Solenoid m_OnDeploy,m_OffDeploy;
 		
-//		Encoder m_LeftEncoder,m_RightEncoder;
+		#ifndef __UseEncoderWorkaround__
+		Encoder m_LeftEncoder,m_RightEncoder;
+		#else
 		//TODO once we move over to beta remove this work-around to see if issue is fixed
+		DigitalInput m_DummyInput; //use only one dummy channel for the busted encoders (as recommended by Joe H)
 		Encoder m_DummyEncoderL,m_LeftEncoder,m_DummyEncoderR,m_RightEncoder;
+		#endif
 		
 		//Servo m_DeployDoor,m_LazySusan;
 		AnalogChannel m_Potentiometer;
