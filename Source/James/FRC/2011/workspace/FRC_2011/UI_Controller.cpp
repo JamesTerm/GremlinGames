be2d203195689d7209c3a3408abb82e9843acc04
james <james@e2c3bcc0-b32a-0410-840c-db224dcf21cb>
2011-10-31 14:42:59 +0000

    Gah! Copy and paste can get you into trouble sometimes! :(

--- Source/James/FRC/2011/workspace/FRC_2011/UI_Controller.cpp
+++ Source/James/FRC/2011/workspace/FRC_2011/UI_Controller.cpp
@@ -80,8 +80,8 @@ UI_Controller::UI_Controller(JoyStick_Binder &joy,AI_Base_Controller *base_contr
 	joy.AddJoy_Button_Default( 3,"Arm_SetPos9feet",false,false,"Joystick_1");
 	joy.AddJoy_Button_Default( 4,"Claw_Grip",true,false,"Joystick_1");
 	joy.AddJoy_Button_Default( 5,"Claw_Squirt",true,false,"Joystick_1");
-	joy.AddJoy_Button_Default( 7,"Claw_Close",false,false,"Joystick_1");
-	joy.AddJoy_Button_Default( 6,"Arm_Rist",false,false,"Joystick_1");
+	joy.AddJoy_Button_Default( 7,"Claw_Close",true,false,"Joystick_1");
+	joy.AddJoy_Button_Default( 6,"Arm_Rist",true,false,"Joystick_1");
 	
 	joy.AddJoy_Button_Default( 8,"Robot_CloseDoor",true,false,"Joystick_1");
 	#endif
@@ -97,8 +97,8 @@ UI_Controller::UI_Controller(JoyStick_Binder &joy,AI_Base_Controller *base_contr
 	joy.AddJoy_Button_Default( 9,"Arm_SetPos9feet",false,false,"Joystick_2");
 	joy.AddJoy_Button_Default( 0,"Claw_Grip",true,false,"Joystick_2");
 	joy.AddJoy_Button_Default( 2,"Claw_Squirt",true,false,"Joystick_2");
-	joy.AddJoy_Button_Default( 8,"Claw_Close",false,false,"Joystick_2");
-	joy.AddJoy_Button_Default( 7,"Arm_Rist",false,false,"Joystick_2");
+	joy.AddJoy_Button_Default( 8,"Claw_Close",true,false,"Joystick_2");
+	joy.AddJoy_Button_Default( 7,"Arm_Rist",true,false,"Joystick_2");
 
 	joy.AddJoy_Button_Default( 7,"Robot_CloseDoor",true,false,"Joystick_1");
 	#endif
