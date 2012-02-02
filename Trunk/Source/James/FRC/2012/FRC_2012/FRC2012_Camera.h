da33c5c4459f0ccbb7169ae85ac27660f745c4e9
james <james@e2c3bcc0-b32a-0410-840c-db224dcf21cb>
2012-02-02 06:01:08 +0000

    Added some research for interfacing with the vision

--- Trunk/Source/James/FRC/2012/FRC_2012/FRC2012_Camera.h
+++ Trunk/Source/James/FRC/2012/FRC_2012/FRC2012_Camera.h
@@ -2,6 +2,36 @@
 
 #define __2011_TestCamera__
 
+class FrameProcessing
+{
+	public:
+		typedef unsigned char BYTE;
+		enum TestChorminance
+		{
+			eTestPb,
+			eTestPr
+		}; 
+	
+		struct XZ_Offset
+		{
+			double X,Z;
+			bool IsError;  //true if error detected
+		};
+	
+		void operator()(HSLImage &image,TestChorminance color=eTestPr,double AspectRatio=4.0/3.0);
+	protected:
+		void TestMinMax(MonoImage &image,BYTE &TestMin,BYTE &TestMax);
+
+	private:
+		//TestChorminance m_TestChorminance;
+		HSLImage *m_TempMsg; //for temp testing;
+		TestChorminance m_ColorToTest;
+		size_t m_Threshold;
+		KalmanFilter m_Dx,m_Dz,m_Ax,m_Az;
+		double m_AspectRatio;
+		int m_XRes,m_YRes;
+};
+
 class FRC_2012_CameraProcessing
 {
 	public:
@@ -11,6 +41,7 @@ class FRC_2012_CameraProcessing
 
 	private:
 		AxisCamera *m_Camera;  //This is a singleton, but treated as a member that is optional
+		FrameProcessing m_FrameProcessor;
 		double m_LastTime;  //Keep track of frame rate
 		int m_Xres,m_Yres;
 };
