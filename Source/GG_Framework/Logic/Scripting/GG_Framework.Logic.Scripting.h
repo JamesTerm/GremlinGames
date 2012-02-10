2f4fd9a7f4db5a120f31ed439b90874beee881ff
james <james@e2c3bcc0-b32a-0410-840c-db224dcf21cb>
2012-02-10 04:18:29 +0000

    Added a new stand-alone version of the LUA... which does a better job of not exposing the LUA symbols.

--- Source/GG_Framework/Logic/Scripting/GG_Framework.Logic.Scripting.h
+++ Source/GG_Framework/Logic/Scripting/GG_Framework.Logic.Scripting.h
@@ -1,6 +1,8 @@
 // GG_Framework.Logic.Scripting.h
 #pragma once
 
+#define __UseStandAloneVersion__
+
 #ifdef FRAMEWORK_LOGIC_SCRIPTING_EXPORTS
 #define FRAMEWORK_LOGIC_SCRIPTING_API __declspec(dllexport)
 #else
@@ -15,14 +17,19 @@
 #include "..\..\..\CompilerSettings.h"
 #include "..\..\Base\GG_Framework.Base.h"
 
+#ifndef __UseStandAloneVersion__
 // We will have to include all of these at once anyway
 extern "C" {
 #include <lua.h>
 #include <lauxlib.h>
 #include <lualib.h>
 }
+#endif
 
 ///////////////////////
 // Fringe.Logic.Scripting Includes
+#ifndef __UseStandAloneVersion__
 #include "Register_C_Functions.h"
+#endif
+
 #include "Script.h"
