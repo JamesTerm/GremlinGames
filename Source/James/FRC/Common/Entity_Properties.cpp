97df6d37eee0ac298da15a4d45ad68446fec259b
james <james@e2c3bcc0-b32a-0410-840c-db224dcf21cb>
2012-02-10 15:22:43 +0000

    Enabled the LUA script... verified that it WORKS!

--- Source/James/FRC/Common/Entity_Properties.cpp
+++ Source/James/FRC/Common/Entity_Properties.cpp
@@ -14,9 +14,16 @@
 #include "Ship_1D.h"
 #include "Ship.h"
 
+#ifndef _countof
+#define _countof(x) sizeof(x)/sizeof(*x)
+#endif
+
 using namespace Framework::Base;
 #define ASSERT(cond) assert(cond);
 #define ASSERT_MSG(cond, msg) if (!(cond)){printf((msg)); assert(cond);}
+
+//namespace Scripting=GG_Framework::Logic::Scripting;
+namespace Scripting=Framework::Scripting;
 
   /***********************************************************************************************************************************/
  /*															Entity1D_Properties														*/
@@ -38,8 +45,7 @@ Entity1D_Properties::Entity1D_Properties(const char EntityName[],double Mass,dou
 	m_IsAngular=IsAngular;
 }
 
-#if 0
-void Entity1D_Properties::LoadFromScript(Framework::Scripting::Script& script)
+void Entity1D_Properties::LoadFromScript(Scripting::Script& script)
 {
 	const char* err;
 
@@ -64,7 +70,6 @@ void Entity1D_Properties::LoadFromScript(Framework::Scripting::Script& script)
 	}
 	script.Pop();
 }
-#endif
 
 void Entity1D_Properties::Initialize(Entity1D *NewEntity) const
 {
@@ -107,7 +112,32 @@ Ship_1D_Properties::Ship_1D_Properties(const char EntityName[], double Mass,doub
 	m_MinRange=MinRange;
 	m_MaxRange=MaxRange;
 	m_UsingRange=UsingRange;
+}
 
+void Ship_1D_Properties::LoadFromScript(Scripting::Script& script)
+{
+	const char* err;
+	m_ShipType=eDefault;
+	m_EntityName="Ship1D";
+	err = script.GetGlobalTable("Ship1D");
+	ASSERT_MSG(!err, err);
+	{
+		err = script.GetField("ACCEL", NULL, NULL, &m_ACCEL);
+		ASSERT_MSG(!err, err);
+		err = script.GetField("BRAKE", NULL, NULL, &m_BRAKE);
+
+		script.GetField("MaxAccelForward", NULL, NULL, &m_MaxAccelForward);
+		script.GetField("MaxAccelReverse", NULL, NULL, &m_MaxAccelReverse);
+
+		err = script.GetField("MAX_SPEED", NULL, NULL, &m_MAX_SPEED);
+		ASSERT_MSG(!err, err);
+	}
+	script.Pop();
+
+	//TODO support range in script
+
+	// Let the base class finish things up
+	__super::LoadFromScript(script);
 }
 
 void Ship_1D_Properties::Initialize(Ship_1D *NewShip) const
@@ -135,6 +165,34 @@ Entity_Properties::Entity_Properties()
 	m_Dimensions[1]=0.9525;
 };
 
+void Entity_Properties::LoadFromScript(Scripting::Script& script)
+{
+	const char* err;
+
+	err = script.GetGlobalTable(m_EntityName.c_str());
+	ASSERT_MSG(!err, err);
+	{
+		err = script.GetField("Mass", NULL, NULL, &m_Mass);
+		ASSERT_MSG(!err, err);
+
+		//Get the ship dimensions
+		err = script.GetFieldTable("Dimensions");
+		if (!err)
+		{
+			//If someone is going through the trouble of providing the dimension field I should expect them to provide all the fields!
+			err = script.GetField("Length", NULL, NULL,&m_Dimensions[1]);
+			ASSERT_MSG(!err, err);
+			err = script.GetField("Width", NULL, NULL,&m_Dimensions[0]);
+			ASSERT_MSG(!err, err);
+			script.Pop();
+		}
+		else
+			m_Dimensions[0]=m_Dimensions[1]=2.0; //using 2.0 here means 1.0 radius default which is a great default for torque radius of mass
+
+	}
+	script.Pop();
+}
+
 void Entity_Properties::Initialize(Entity2D *NewEntity) const
 {
 	NewEntity->m_Dimensions[0]=m_Dimensions[0];
@@ -145,6 +203,14 @@ void Entity_Properties::Initialize(Entity2D *NewEntity) const
   /***********************************************************************************************************************************/
  /*															Ship_Properties															*/
 /***********************************************************************************************************************************/
+//These must be in the same order as they are in Ship_Properties::Ship_Type
+const char * const csz_RobotNames[] =
+{
+	"RobotTank",
+	"RobotSwerve",
+	"Robot2011",
+	"Robot2012",
+};
 
 Ship_Properties::Ship_Properties()
 {
@@ -175,6 +241,86 @@ Ship_Properties::Ship_Properties()
 	m_EngineDeceleration= m_ACCEL/RAMP_DOWN_DUR;
 };
 
+void Ship_Properties::LoadFromScript(Scripting::Script& script)
+{
+	const char* err;
+	//m_ShipType=eDefault;
+	m_EntityName="Ship";
+	err = script.GetGlobalTable("Ship");
+	if (err)
+	{
+		for (size_t i=0;i<_countof(csz_RobotNames);i++)
+		{
+			err = script.GetGlobalTable(csz_RobotNames[i]);
+			if (!err)
+			{
+				//m_ShipType=(Ship_Type)(i+1);
+				m_EntityName=csz_RobotNames[i];
+				break;
+			}
+		}
+	}
+	ASSERT_MSG(!err, err);
+	{
+		double dHeading;
+		err = script.GetField("dHeading", NULL, NULL, &dHeading);
+		m_dHeading=DEG_2_RAD(dHeading);
+		ASSERT_MSG(!err, err);
+
+		err = script.GetField("ACCEL", NULL, NULL, &m_ACCEL);
+		ASSERT_MSG(!err, err);
+		err = script.GetField("BRAKE", NULL, NULL, &m_BRAKE);
+		ASSERT_MSG(!err, err);
+		err = script.GetField("STRAFE", NULL, NULL, &m_STRAFE);
+		if (err)
+			m_STRAFE=m_BRAKE; 		//Give strafe the default of the brake
+
+		m_AFTERBURNER_ACCEL=m_ACCEL;  //Give afterburner acceleration rate the same as thrusters for default
+		err = script.GetField("AFTERBURNER_ACCEL", NULL, NULL, &m_AFTERBURNER_ACCEL);
+
+		m_AFTERBURNER_BRAKE=m_BRAKE;  //Give afterburner brake rate the same as brakes for default
+		err = script.GetField("AFTERBURNER_ACCEL", NULL, NULL, &m_AFTERBURNER_BRAKE);
+
+		// By defaults, we want all of the engines to ramp up to full in this amount of time
+		double RAMP_UP_DUR = 1.0;
+		double RAMP_DOWN_DUR = 1.0;
+
+		err = script.GetField("RAMP_UP_DUR", NULL, NULL, &RAMP_UP_DUR);
+		err = script.GetField("RAMP_DOWN_DUR", NULL, NULL, &RAMP_DOWN_DUR);
+		const double MIN_RAMP = 0.00001;	// Avoid stupid numbers, div by 0 errors, etc.
+		if (RAMP_UP_DUR < MIN_RAMP) RAMP_UP_DUR = MIN_RAMP;	
+		if (RAMP_DOWN_DUR < MIN_RAMP) RAMP_DOWN_DUR = MIN_RAMP;
+
+		err = script.GetField("EngineDeceleration", NULL, NULL, &m_EngineDeceleration);
+		if (err) m_EngineDeceleration= m_ACCEL/RAMP_DOWN_DUR;
+		err = script.GetField("EngineRampStrafe", NULL, NULL, &m_EngineRampStrafe);
+		if (err) m_EngineRampStrafe= m_STRAFE/RAMP_UP_DUR;
+		err = script.GetField("EngineRampForward", NULL, NULL, &m_EngineRampForward);
+		if (err) m_EngineRampForward= m_ACCEL/RAMP_UP_DUR;
+		err = script.GetField("EngineRampReverse", NULL, NULL, &m_EngineRampReverse);
+		if (err) m_EngineRampReverse= m_BRAKE/RAMP_UP_DUR;
+		err = script.GetField("EngineRampAfterBurner", NULL, NULL, &m_EngineRampAfterBurner);
+		if (err) m_EngineRampAfterBurner= m_AFTERBURNER_ACCEL/RAMP_UP_DUR;
+
+
+		script.GetField("MaxAccelLeft", NULL, NULL, &m_MaxAccelLeft);
+		script.GetField("MaxAccelRight", NULL, NULL, &m_MaxAccelRight);
+		script.GetField("MaxAccelForward", NULL, NULL, &m_MaxAccelForward);
+		script.GetField("MaxAccelReverse", NULL, NULL, &m_MaxAccelReverse);
+		script.GetField("MaxTorqueYaw", NULL, NULL, &m_MaxTorqueYaw);
+
+		err = script.GetField("MAX_SPEED", NULL, NULL, &m_MAX_SPEED);
+		ASSERT_MSG(!err, err);
+		err = script.GetField("ENGAGED_MAX_SPEED", NULL, NULL, &m_ENGAGED_MAX_SPEED);
+		if (err)
+			m_ENGAGED_MAX_SPEED=m_MAX_SPEED;
+
+	}
+	script.Pop();
+
+	// Let the base class finish things up
+	__super::LoadFromScript(script);
+}
 
 void Ship_Properties::Initialize(Ship_2D *NewShip) const
 {
