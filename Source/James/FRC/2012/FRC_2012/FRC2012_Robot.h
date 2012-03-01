a22082a7e52871fac2e13f5cee73196e94e6a3ec
james <james@e2c3bcc0-b32a-0410-840c-db224dcf21cb>
2012-03-01 05:06:02 +0000

    Added ability to script error correction offsets to the ramps

--- Source/James/FRC/2012/FRC_2012/FRC2012_Robot.h
+++ Source/James/FRC/2012/FRC_2012/FRC2012_Robot.h
@@ -30,6 +30,13 @@ public:
 	size_t Coordinates_DiplayRow;
 	size_t TargetVars_DisplayRow;
 	size_t PowerVelocity_DisplayRow;
+
+	struct Autonomous_Properties
+	{
+		Vec2D RampLeft_ErrorCorrection_Offset;
+		Vec2D RampRight_ErrorCorrection_Offset;
+		Vec2D RampCenter_ErrorCorrection_Offset;
+	} Autonomous_Props;
 };
 
 class FRC_2012_Robot_Properties : public Tank_Robot_Properties
@@ -229,6 +236,7 @@ class FRC_2012_Robot : public Tank_Robot
 		void SetPresetPosition(size_t index,bool IgnoreOrientation=false);
 		void Set_Auton_PresetPosition(size_t index);
 		void SetTarget(Targets target);
+		const FRC_2012_Robot_Properties &GetRobotProps() const;
 	protected:
 		virtual void ComputeDeadZone(double &LeftVoltage,double &RightVoltage);
 		virtual void BindAdditionalEventControls(bool Bind);
