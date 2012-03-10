6fcfadfc9befb2292a7c1d933d1ae4c7300fa3f1
cary <cary@e2c3bcc0-b32a-0410-840c-db224dcf21cb>
2012-03-10 22:40:30 +0000

    Added alternative solenoid flippers

--- Source/James/FRC/2012/FRC_2012/FRC2012_InOut_Interface.h
+++ Source/James/FRC/2012/FRC_2012/FRC2012_InOut_Interface.h
@@ -12,6 +12,7 @@ class FRC_2012_Robot_Control : public FRC_2012_Control_Interface
 		//pitch ramp is using i2c
 		Compressor m_Compress;
 		Solenoid m_OnLowGear,m_OffLowGear;
+		Solenoid m_FlipperDown,m_FlipperUp;
 		Solenoid m_OnRampDeployment,m_OffRampDeployment;
 		Relay m_LowerConveyor_Relay,m_MiddleConveyor_Relay,m_FireConveyor_Relay;
 		
