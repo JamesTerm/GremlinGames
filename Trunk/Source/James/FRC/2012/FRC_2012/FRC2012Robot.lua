fa3234f6777a44ba58effa92552dc418d96cc0f1
james <james@e2c3bcc0-b32a-0410-840c-db224dcf21cb>
2012-03-21 15:19:48 +0000

    Added ability to square voltage (displays voltage on PID dump for angular)

--- Trunk/Source/James/FRC/2012/FRC_2012/FRC2012Robot.lua
+++ Trunk/Source/James/FRC/2012/FRC_2012/FRC2012Robot.lua
@@ -114,10 +114,11 @@ MainRobot = {
 			show_pid_dump='no',
 			ds_display_row=-1,				--Use this display to determine max speed (try to get a good match)
 			pid=
-			{p=1, i=0, d=0},
-			tolerance=0.1,					--we need decent precision (this will depend on ramp up time too)
+			{p=1, i=1, d=0},
+			tolerance=5.0,					--we need decent precision (this will depend on ramp up time too)
 			encoder_to_wheel_ratio=1.0,     --Just use the gearing ratios here
 			voltage_multiply=1.0,
+			square_voltage='yes',
 
 			length_in=6,					--6 inch diameter (we shouldn't worry about tweaking this just measure it and be done)
 			max_speed=(5000.0/60.0) * Pi2,	--(This is clocked at 5000 rpm) in radians
