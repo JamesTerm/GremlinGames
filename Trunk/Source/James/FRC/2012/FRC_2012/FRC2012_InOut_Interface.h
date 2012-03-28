32a0f13f424a627a90edc158608cfc3e37698663
james <james@e2c3bcc0-b32a-0410-840c-db224dcf21cb>
2012-03-28 15:32:25 +0000

    re-enabled the Kalman filter after discovering that it had a bug in the NAN detection.

--- Trunk/Source/James/FRC/2012/FRC_2012/FRC2012_InOut_Interface.h
+++ Trunk/Source/James/FRC/2012/FRC_2012/FRC2012_InOut_Interface.h
@@ -32,7 +32,7 @@ class FRC_2012_Robot_Control : public FRC_2012_Control_Interface
 	private:
 		//probably will not need these
 		//KalmanFilter m_KalFilter_Arm,m_KalFilter_EncodeLeft,m_KalFilter_EncodeRight;
-		//KalmanFilter m_PowerWheelFilter;
+		KalmanFilter m_PowerWheelFilter;
 		Blend_Averager<double> m_PowerWheelAverager;
 		
 		//Note these are temporary to avoid flooding, and should be removed once they are no longer needed
