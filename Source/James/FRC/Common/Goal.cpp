a159be69c13288d197d8edd96f1db3ff465e0d40
james <james@e2c3bcc0-b32a-0410-840c-db224dcf21cb>
2012-04-10 04:10:10 +0000

    Added a wait all option for the multitask goal... This can be used in 2012 auton where the balls either trigger a fired condition or the timeout happens... whichever comes first.

--- Source/James/FRC/Common/Goal.cpp
+++ Source/James/FRC/Common/Goal.cpp
@@ -70,6 +70,9 @@ Goal::Goal_Status CompositeGoal::ProcessSubgoals(double dTime_s)
  /*												MultitaskGoal													*/
 /***************************************************************************************************************/
 
+MultitaskGoal::MultitaskGoal(bool WaitAll) : m_WaitAll(WaitAll)
+{
+}
 
 void MultitaskGoal::RemoveAllGoals()
 {
@@ -96,13 +99,24 @@ Goal::Goal_Status MultitaskGoal::Process(double dTime_s)
 	ActivateIfInactive();
 	Goal_Status status=eFailed;
 	size_t NonActiveCount=0;
+
+	bool SuccessDetected=false;
+	//To keep things simple we'll always run a complete iteration of all the goals for the given process cycle, and simply or in the success
+	//detected... This way any success that happened will be reflected and dealt with below
 	for (GoalList::iterator it = m_GoalsToProcess.begin(); it!=m_GoalsToProcess.end(); ++it)
 	{
 		status=(*it)->Process(dTime_s);
 		if (status!=eActive)
+		{
 			NonActiveCount++;
+			SuccessDetected|=(status==eCompleted);
+		}
 	}
-	if (NonActiveCount<m_GoalsToProcess.size())
+
+	//Either we wait until no more active goals exist, or if the wait all option is false, we can complete when the first detected successful completion
+	//has occurred.  So for that... if no successful completion then it would fall back to the wait all logic and then evaluate if it failed
+	const bool IsCompleted=((NonActiveCount>=m_GoalsToProcess.size()) || ((!m_WaitAll)&&(SuccessDetected)));
+	if (!IsCompleted)
 		m_Status=eActive;
 	else
 	{
