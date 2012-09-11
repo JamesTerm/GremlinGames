2a9b3d461beac58f2e0177a0dc41b165d368c641
james <james@e2c3bcc0-b32a-0410-840c-db224dcf21cb>
2012-09-11 20:22:43 +0000

    added omni wheel UI with the kicker

--- Source/James/AI_Tester/AI_Tester/Nona_Robot.cpp
+++ Source/James/AI_Tester/AI_Tester/Nona_Robot.cpp
@@ -18,7 +18,9 @@ using namespace std;
 namespace Scripting=GG_Framework::Logic::Scripting;
 //namespace Scripting=Framework::Scripting;
 
+//const double PI=M_PI;
 const double Pi2=M_PI*2.0;
+const double Pi_Half=1.57079632679489661923;
 
 
   /***********************************************************************************************************/
@@ -184,4 +186,196 @@ void Nona_Robot_Control::Swerve_Drive_Control_TimeChange(double dTime_s)
 			m_EncoderVoltage[Swerve_Robot::eWheel_RL],m_EncoderVoltage[Swerve_Robot::eWheel_RR],m_KickerWheelVoltage);
 	}
 	__super::Swerve_Drive_Control_TimeChange(dTime_s);
+}
+
+
+  /***************************************************************************************************************/
+ /*												Omni_Wheel_UI													*/
+/***************************************************************************************************************/
+
+void Omni_Wheel_UI::Initialize(Entity2D::EventMap& em, const Wheel_Properties *props)
+{
+	m_props=*props;
+	m_Swivel=0.0;
+	m_Rotation=0.0;
+}
+
+void Omni_Wheel_UI::UI_Init(Actor_Text *parent) 
+{
+	m_UIParent=parent;
+
+	osg::Vec3 position(0.5*c_Scene_XRes_InPixels,0.5*c_Scene_YRes_InPixels,0.0f);
+	m_Front= new osgText::Text;
+	m_Front->setColor(osg::Vec4(0.3,0.3,1.0,1.0));
+	m_Front->setCharacterSize(m_UIParent->GetFontSize());
+	m_Front->setFontResolution(10,10);
+	m_Front->setPosition(position);
+	m_Front->setAlignment(osgText::Text::CENTER_CENTER);
+	m_Front->setText(L"U");
+	m_Front->setUpdateCallback(m_UIParent);
+
+	m_Back= new osgText::Text;
+	m_Back->setColor(osg::Vec4(0.1,0.1,1.0,1.0));
+	m_Back->setCharacterSize(m_UIParent->GetFontSize());
+	m_Back->setFontResolution(10,10);
+	m_Back->setPosition(position);
+	m_Back->setAlignment(osgText::Text::CENTER_CENTER);
+	m_Back->setText(L"U");
+	m_Back->setUpdateCallback(m_UIParent);
+
+	m_Tread= new osgText::Text;
+	m_Tread->setColor(osg::Vec4(1.0,1.0,1.0,1.0));
+	m_Tread->setCharacterSize(m_UIParent->GetFontSize());
+	m_Tread->setFontResolution(10,10);
+	m_Tread->setPosition(position);
+	m_Tread->setAlignment(osgText::Text::CENTER_CENTER);
+	//m_Tread->setText(L"\"");
+	m_Tread->setText(L"__");
+	m_Tread->setUpdateCallback(m_UIParent);
+
+}
+
+void Omni_Wheel_UI::UpdateScene (osg::Geode *geode, bool AddOrRemove)
+{
+	if (AddOrRemove)
+	{
+		if (m_Front.valid()) geode->addDrawable(m_Front);
+		if (m_Back.valid()) geode->addDrawable(m_Back);
+		if (m_Tread.valid()) geode->addDrawable(m_Tread);
+	}
+	else
+	{
+		if (m_Front.valid()) geode->removeDrawable(m_Front);
+		if (m_Back.valid()) geode->removeDrawable(m_Back);
+		if (m_Tread.valid()) geode->removeDrawable(m_Tread);
+	}
+}
+
+void Omni_Wheel_UI::update(osg::NodeVisitor *nv, osg::Drawable *draw,const osg::Vec3 &parent_pos,double Heading)
+{
+	const double FS=m_UIParent->GetFontSize();
+	Vec2d FrontSwivel(0.0,0.5);
+	Vec2d BackSwivel(0.0,-0.5);
+	//Vec2d TreadRotPos(0.0,cos(m_Rotation)-0.3);
+	Vec2d TreadRotPos(sin(m_Rotation)*((fabs(m_Swivel)>PI_2)?0.5:-0.5),(cos(m_Rotation)*.8)+0.5);
+	FrontSwivel=GlobalToLocal(m_Swivel,FrontSwivel);
+	BackSwivel=GlobalToLocal(m_Swivel,BackSwivel);
+	TreadRotPos=GlobalToLocal(m_Swivel,TreadRotPos);
+
+	const Vec2d frontOffset(m_props.m_Offset[0]+FrontSwivel[0],m_props.m_Offset[1]+FrontSwivel[1]);
+	const Vec2d backOffset(m_props.m_Offset[0]+BackSwivel[0],m_props.m_Offset[1]+BackSwivel[1]);
+	const Vec2d TreadOffset(m_props.m_Offset[0]+TreadRotPos[0],m_props.m_Offset[1]+TreadRotPos[1]);
+
+	const Vec2d FrontLocalOffset=GlobalToLocal(Heading,frontOffset);
+	const Vec2d BackLocalOffset=GlobalToLocal(Heading,backOffset);
+	const Vec2d TreadLocalOffset=GlobalToLocal(Heading,TreadOffset);
+	const osg::Vec3 frontPos(parent_pos[0]+(FrontLocalOffset[0]*FS),parent_pos[1]+(FrontLocalOffset[1]*FS),parent_pos[2]);
+	const osg::Vec3 backPos (parent_pos[0]+( BackLocalOffset[0]*FS),parent_pos[1]+( BackLocalOffset[1]*FS),parent_pos[2]);
+	const osg::Vec3 TreadPos (parent_pos[0]+( TreadLocalOffset[0]*FS),parent_pos[1]+( TreadLocalOffset[1]*FS),parent_pos[2]);
+
+	const double TreadColor=((sin(-m_Rotation) + 1.0)/2.0) * 0.8 + 0.2;
+	m_Tread->setColor(osg::Vec4(TreadColor,TreadColor,TreadColor,1.0));
+
+	if (m_Front.valid())
+	{
+		m_Front->setPosition(frontPos);
+		m_Front->setRotation(FromLW_Rot_Radians(PI+Heading+m_Swivel,0.0,0.0));
+	}
+	if (m_Back.valid())
+	{
+		m_Back->setPosition(backPos);
+		m_Back->setRotation(FromLW_Rot_Radians(Heading+m_Swivel,0.0,0.0));
+	}
+	if (m_Tread.valid())
+	{
+		m_Tread->setPosition(TreadPos);
+		m_Tread->setRotation(FromLW_Rot_Radians(Heading+m_Swivel,0.0,0.0));
+	}
+}
+
+void Omni_Wheel_UI::Text_SizeToUse(double SizeToUse)
+{
+	if (m_Front.valid())	m_Front->setCharacterSize(SizeToUse);
+	if (m_Back.valid()) m_Back->setCharacterSize(SizeToUse);
+	if (m_Tread.valid()) m_Tread->setCharacterSize(SizeToUse);
+}
+
+void Omni_Wheel_UI::AddRotation(double RadiansToAdd)
+{
+	m_Rotation+=RadiansToAdd;
+	if (m_Rotation>Pi2)
+		m_Rotation-=Pi2;
+	else if (m_Rotation<-Pi2)
+		m_Rotation+=Pi2;
+}
+
+  /***************************************************************************************************************/
+ /*												Nona_Robot_UI													*/
+/***************************************************************************************************************/
+void Nona_Robot_UI::UI_Init(Actor_Text *parent)
+{
+	for (size_t i=0;i<5;i++)
+		m_Wheel[i].UI_Init(parent);
+}
+
+void Nona_Robot_UI::Initialize(Entity2D::EventMap& em, const Entity_Properties *props)
+{
+	Vec2D Offsets[5]=
+	{
+		Vec2D(-1.6, 2.0),
+		Vec2D( 1.6, 2.0),
+		Vec2D(-1.6,-2.0),
+		Vec2D( 1.6,-2.0),
+		Vec2D( 0.0,0.0),
+	};
+	for (size_t i=0;i<5;i++)
+	{
+		Omni_Wheel_UI::Wheel_Properties props;
+		props.m_Offset=Offsets[i];
+		props.m_Wheel_Diameter=m_NonaRobot->GetSwerveRobotProps().WheelDiameter;
+		m_Wheel[i].Initialize(em,&props);
+	}
+}
+
+void Nona_Robot_UI::custom_update(osg::NodeVisitor *nv, osg::Drawable *draw,const osg::Vec3 &parent_pos)
+{
+	//just dispatch the update to the wheels (for now)
+	for (size_t i=0;i<5;i++)
+		m_Wheel[i].update(nv,draw,parent_pos,-(m_NonaRobot->GetAtt_r()));
+}
+
+void Nona_Robot_UI::Text_SizeToUse(double SizeToUse)
+{
+	for (size_t i=0;i<5;i++)
+		m_Wheel[i].Text_SizeToUse(SizeToUse);
+}
+
+void Nona_Robot_UI::UpdateScene (osg::Geode *geode, bool AddOrRemove)
+{
+	for (size_t i=0;i<5;i++)
+		m_Wheel[i].UpdateScene(geode,AddOrRemove);
+}
+
+void Nona_Robot_UI::TimeChange(double dTime_s)
+{
+	Nona_Robot &_=*m_NonaRobot;
+	for (size_t i=0;i<4;i++)
+	{
+		//TODO GetIntendedVelocities for intended UI
+		m_Wheel[i].SetSwivel(_.GetSwerveVelocitiesFromIndex(i+4));
+		//For the linear velocities we'll convert to angular velocity and then extract the delta of this slice of time
+		//const double LinearVelocity=_.GetSwerveVelocitiesFromIndex(i);
+		const double LinearVelocity=const_cast<Swerve_Drive_Control_Interface &>(_.GetRobotControl()).GetRotaryCurrentPorV(i);
+		const double PixelHackScale=m_Wheel[i].GetFontSize()/8.0;  //scale the wheels to be pixel aesthetic
+		const double RPS=LinearVelocity /  (PI * _.GetSwerveRobotProps().WheelDiameter * PixelHackScale);
+		const double AngularVelocity=RPS * Pi2;
+		m_Wheel[i].AddRotation(AngularVelocity*dTime_s);
+	}
+	//Setup the kicker wheel
+	m_Wheel[4].SetSwivel(Pi_Half);
+	const double LinearVelocity=const_cast<Swerve_Drive_Control_Interface &>(_.GetRobotControl()).GetRotaryCurrentPorV(Nona_Robot::eWheel_Kicker);
+	const double PixelHackScale=m_Wheel[4].GetFontSize()/8.0;  //scale the wheels to be pixel aesthetic
+	const double RPS=LinearVelocity /  (PI * _.GetSwerveRobotProps().WheelDiameter * PixelHackScale);
+	const double AngularVelocity=RPS * Pi2;
+	m_Wheel[4].AddRotation(AngularVelocity*dTime_s);
 }
