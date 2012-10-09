1716829e9c5c13a2740243bc47ed82c9f7192515
james <james@e2c3bcc0-b32a-0410-840c-db224dcf21cb>
2012-10-09 19:32:25 +0000

    Added methods to extract centripetal force and magnitude

--- Trunk/Source/James/FRC/Common/Physics_2D.h
+++ Trunk/Source/James/FRC/Common/Physics_2D.h
@@ -4,7 +4,7 @@ class PhysicsEntity_2D
 {
 	public:
 		typedef Framework::Base::Vec2d Vec2D;  //Tidy up the declaration to look neat
-		
+		//typedef osg::Vec2d Vec2D;
 		PhysicsEntity_2D();
 		virtual ~PhysicsEntity_2D() {}
 		
@@ -84,18 +84,24 @@ class PhysicsEntity_2D
 		virtual Vec2D GetForceFromVelocity( 
 			const Vec2D &vDesiredVelocity,	///< How fast do you want to go (in a specific direction)
 			double DeltaTime_s					///< How quickly do you want to get there
-			);
+			) const;
 		virtual double GetTorqueFromVelocity( 
 			double vDesiredVelocity,	///< How fast do you want to go (in a specific direction)
 			double DeltaTime_s					///< How quickly do you want to get there (usually in time slices)
-			);
+			) const;
 
 		///This is a clean way to compute how much torque that can be applied given the maximum amount available (e.g. thruster capacity)
 		///It should be noted that this treats roll as a separate factor, which is best suited for avionic type of context
 		/// \Note all restraint parameters are positive (i.e. ForceRestraintNegative)
 		double ComputeRestrainedTorque(double Torque,double TorqueRestraint,double dTime_s);
 		Vec2D ComputeRestrainedForce(const Vec2D &LocalForce,const Vec2D &ForceRestraintPositive,const Vec2D &ForceRestraintNegative,double dTime_s);
-
+		//This returns in the form of magnitude using the proper equations
+		static double GetCentripetalAcceleration(double LinearVelocity,double AngularVelocity,double DeltaTime_s);
+		double GetCentripetalAcceleration(double DeltaTime_s) const;
+		///This returns the global acceleration needed to maintain linear velocity
+		Vec2D GetCentripetalAcceleration_2D(double DeltaTime_s) const;
+		///This returns the global force needed to maintain the current linear velocity
+		Vec2D GetCentripetalForce(double DeltaTime_s) const;
 	protected:
 		double m_EntityMass;
 		double m_StaticFriction,m_KineticFriction;
@@ -116,6 +122,9 @@ class PhysicsEntity_2D
 class FlightDynamics_2D : public PhysicsEntity_2D
 {
 	public:
+		typedef Framework::Base::Vec2d Vec2D;
+		//typedef osg::Vec2d Vec2D;
+
 		//provide common area to initialize members
 		void init();
 		FlightDynamics_2D();
