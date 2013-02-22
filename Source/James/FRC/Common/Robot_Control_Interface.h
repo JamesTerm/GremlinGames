0a019fc19013774bdac73a4aededfdf02441d37e
james <james@e2c3bcc0-b32a-0410-840c-db224dcf21cb>
2013-02-22 18:50:41 +0000

    Added Servo system

--- Source/James/FRC/Common/Robot_Control_Interface.h
+++ Source/James/FRC/Common/Robot_Control_Interface.h
@@ -46,3 +46,20 @@ public:
 	virtual double GetRotaryCurrentPorV(size_t index=0)=0;
 	virtual void UpdateRotaryVoltage(size_t index,double Voltage)=0;
 };
+
+class Servo_Control_Interface
+{
+	public:
+		virtual void Reset_Servo(size_t index=0)=0; 
+		///Get the servo angle.
+		///Assume that the servo angle is linear with respect to the PWM value (big assumption, need to test).
+		/// \return The angle in radians to which the servo is set.
+		virtual double GetServoAngle(size_t index=0)=0;
+		/// Set the servo angle.
+		/// Assume that the servo angle is linear with respect to the PWM value (big assumption, need to test).
+		/// Servo angles that are out of the supported range of the servo simply "saturate" in that direction
+		/// In other words, if the servo has a range of (X degrees to Y degrees) than angles of less than X
+		/// result in an angle of X being set and angles of more than Y degrees result in an angle of Y being set.
+		///	\param degrees The angle in degrees to set the servo.
+		virtual void SetServoAngle(size_t index,double radians)=0;
+};
