4ff840b047aed9847e9020a9ccf8490c03b41a73
james <james@e2c3bcc0-b32a-0410-840c-db224dcf21cb>
2013-03-08 11:13:46 +0000

    Added auton scripting

--- Trunk/Source/James/FRC/2013/lua/FRC2013/Competition/FRC2013Robot.lua
+++ Trunk/Source/James/FRC/2013/lua/FRC2013/Competition/FRC2013Robot.lua
@@ -102,6 +102,24 @@ MainRobot = {
 			c61={p=1.0, y=1.0}, c62={p=1.0, y=1.0}, c63={p=1.0, y=1.0},
 		},
 	
+		auton =
+		{
+			--If you put -1.0 for the timeout wait it will wait infinitely (good for initial testing or if we are not tipping ramps)
+			--ball 1 initial wait should be long enough for a good ramp up from zero speed
+			ball_1 ={initial_wait=  2.0, tolerance=75.0, timeout_wait=4.0},
+			--ball 2 initial wait should be long enough to recover from dip and short enough to be active during second ball's deployment
+			ball_2 ={initial_wait=0.500, tolerance=75.0, timeout_wait=4.0},
+			--panic mode incase the wait ball doesn't work... using zero makes it work like before just pure time
+			--ball_1 ={initial_wait=  3.5, tolerance=0.0, timeout_wait=-1.0},
+			--ball_2 ={initial_wait=  3.5, tolerance=0.0, timeout_wait=-1.0},
+			
+			init_rev=2.0,
+			wait_on_times=1.0,
+			wait_off_times=1.0,
+			first_stage_speed=(3804.55/60.0) * Pi2,
+			second_stage_speed=(3804.55/60.0) * Pi2
+		},
+
 		climb_1 =
 		{
 			lift_ft=2,
