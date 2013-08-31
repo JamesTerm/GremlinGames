e20788f131a665d5ac732deea645306a66d03f37
james <james@e2c3bcc0-b32a-0410-840c-db224dcf21cb>
2013-08-31 04:30:14 +0000

    Added a good test case number for SmartDashboard, and made disabled case where it can be inert.

--- Source/James/AI_Tester/AI_Tester/AI_Tester.h
+++ Source/James/AI_Tester/AI_Tester/AI_Tester.h
@@ -5,7 +5,36 @@
 #include "../../../GG_Framework/Logic/Scripting/GG_Framework.Logic.Scripting.h"
 
 #define AI_TesterCode  //used to branch AI test code from wind-river code
+
 #define __DisableSmartDashboard__ //used to quickly disable the smart dashboard
+#ifndef __DisableSmartDashboard__
+#include "../../SmartDashboard/SmartDashboard/SmartDashboard.h"
+#else
+class SmartDashboard //: public SensorBase
+{
+public:
+	static void init() {}
+	static void shutdown() {}
+
+	//static void PutData(std::string key, Sendable *data) {}
+	//static void PutData(NamedSendable *value){}
+	//static Sendable* GetData(std::string keyName);
+
+	static void PutBoolean(std::string keyName, bool value) {}
+	static bool GetBoolean(std::string keyName) {return false;}
+
+	static void PutNumber(std::string keyName, double value) {}
+	static double GetNumber(std::string keyName) {return 0.0;}
+
+	static void PutString(std::string keyName, std::string value) {}
+	static int GetString(std::string keyName, char *value, unsigned int valueLen) {return 0;}
+	static std::string GetString(std::string keyName) {return "";} 
+
+	//static void PutValue(std::string keyName, ComplexData& value) {}
+	//static void RetrieveValue(std::string keyName, ComplexData& value) {}
+};
+#endif
+
 typedef osg::Vec2d Vec2D;
 namespace Base=GG_Framework::Base;
 namespace UI=GG_Framework::UI;
