d805fe97f1236299602d5d9f499425ebbafaf3c1
james <james@e2c3bcc0-b32a-0410-840c-db224dcf21cb>
2013-09-05 18:38:37 +0000

    Added SmartDashboard2 which has NetworkTables in its native git repository environment and up to date.

---
+++ Source/James/SmartDashboard2/NetworkTables/cpp/include/src/main/include/networktables2/thread/DefaultThreadManager.h
@@ -0,0 +1,68 @@
+/*
+ * DefaultThreadManager.h
+ * Desktop
+ *
+ *  Created on: Sep 21, 2012
+ *      Author: Mitchell Wills
+ */
+
+#ifndef DEFAULTTHREADMANAGER_H_
+#define DEFAULTTHREADMANAGER_H_
+
+
+class DefaultThreadManager;
+class PeriodicNTThread;
+
+#include "networktables2/thread/PeriodicRunnable.h"
+#include "networktables2/thread/NTThreadManager.h"
+#include "networktables2/thread/NTThread.h"
+
+#ifdef __vxworks
+
+#include "OSAL/Task.h"
+
+class DefaultThreadManager : public NTThreadManager{
+public:
+	virtual NTThread* newBlockingPeriodicThread(PeriodicRunnable* r, const char* name);
+};
+
+class PeriodicNTThread : public NTThread {
+private:
+	const char* name;
+	NTTask* thread;
+	PeriodicRunnable* r;
+	bool run;
+	int _taskMain();
+	static int taskMain(PeriodicNTThread* o);
+public:
+	PeriodicNTThread(PeriodicRunnable* r, const char* name);
+	virtual ~PeriodicNTThread();
+	virtual void stop();
+	virtual bool isRunning();
+};
+
+#else
+
+#include <pthread.h>
+
+class DefaultThreadManager : public NTThreadManager{
+	virtual NTThread* newBlockingPeriodicThread(PeriodicRunnable* r, const char* name);
+};
+
+class PeriodicNTThread : public NTThread {
+private:
+	pthread_t thread;
+	PeriodicRunnable* r;
+	bool run;
+	void _taskMain();
+	static void* taskMain(PeriodicNTThread* o);
+public:
+	PeriodicNTThread(PeriodicRunnable* r, const char* name);
+	virtual ~PeriodicNTThread();
+	virtual void stop();
+	virtual bool isRunning();
+};
+#endif // __vxworks
+
+
+#endif /* DEFAULTTHREADMANAGER_H_ */
