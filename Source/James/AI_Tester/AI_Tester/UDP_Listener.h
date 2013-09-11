c5eb209b9157624c022669e22abb2a819ea6cb82
james <james@e2c3bcc0-b32a-0410-840c-db224dcf21cb>
2013-09-11 21:32:19 +0000

    Added SmartDashboard as the method for receiving camera deltas

--- Source/James/AI_Tester/AI_Tester/UDP_Listener.h
+++ Source/James/AI_Tester/AI_Tester/UDP_Listener.h
@@ -7,17 +7,35 @@ class UDP_Listener_Interface
 		virtual void ProcessPacket(char *pkt,size_t pkt_size)=0;
 };
 
-class coodinate_manager_Interface : public UDP_Listener_Interface
+class coodinate_manager_Interface
 {
-	protected:
-		double m_Xpos,m_Ypos;
-		bool m_Updated; //true if we received a packet for this slice of time
 	public:
-		static coodinate_manager_Interface *CreateInstance();
+		enum ListeningPlatform
+		{
+			eListeningPlatform_UDP,
+			eListeningPlatform_TCPIP,
+		};
+		static coodinate_manager_Interface *CreateInstance(ListeningPlatform listeningPlatform);
 		static void DestroyInstance(coodinate_manager_Interface *instance);
+
+		virtual ~coodinate_manager_Interface() {}
 		virtual void TimeChange(double dTime_s)=0;
 
-		__inline double GetXpos() const {return m_Xpos;}
-		__inline double GetYpos() const {return m_Ypos;}
-		__inline bool IsUpdated() const {return m_Updated;}
+		virtual double GetXpos() const =0;
+		virtual double GetYpos() const =0;
+		virtual bool IsUpdated() const =0;
+};
+
+class coodinate_manager :	public UDP_Listener_Interface,
+							public coodinate_manager_Interface
+{
+protected:
+	double m_Xpos,m_Ypos;
+	bool m_Updated; //true if we received a packet for this slice of time
+protected:  //from coodinate_manager_Interface
+	virtual void TimeChange(double dTime_s)=0;
+
+	virtual double GetXpos() const {return m_Xpos;}
+	virtual double GetYpos() const {return m_Ypos;}
+	virtual bool IsUpdated() const {return m_Updated;}
 };
