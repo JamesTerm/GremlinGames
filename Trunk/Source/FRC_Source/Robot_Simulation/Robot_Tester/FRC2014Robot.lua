5aa681195ddc8c75a4417c2b0ff1f85fa9f5e9d6
james <james@e2c3bcc0-b32a-0410-840c-db224dcf21cb>
2014-01-11 16:00:04 +0000

    Stripped out most all 2012 specifics that will not be needed for 2014

--- Trunk/Source/FRC_Source/Robot_Simulation/Robot_Tester/FRC2014Robot.lua
+++ Trunk/Source/FRC_Source/Robot_Simulation/Robot_Tester/FRC2014Robot.lua
@@ -16,39 +16,6 @@ LowGearSpeed  = (167.06 / 60.0) * Pi * FRC2014_wheel_diameter_in * Inches2Meters
 Drive_MaxAccel=4
 skid=math.cos(math.atan2(WheelBase_Width_In,WheelBase_Width_In))
 
---CIM Motor torque
-CIM_StallTorque_OzIn=343.3
-CIM_StallTorque_Nm=CIM_StallTorque_OzIn*OunceInchToNewton
-CIM_MaxRPM=5310
-CIM_Vel_To_Torque_oz=(1.0/(CIM_MaxRPM/60.0)) * CIM_StallTorque_OzIn
-CIM_Vel_To_Torque_nm=CIM_Vel_To_Torque_oz*OunceInchToNewton
-CIM_MotorTorque=(CIM_MaxRPM / 60) * CIM_Vel_To_Torque_nm  --long math to show its equal to CIM_StallTorque_Nm
-
-DriveTrain_GearReduction = 12.4158
-DriveTrain_Efficiency=1.0
-DriveTrain_MaxTorque = 2.0 * CIM_MotorTorque * DriveTrain_GearReduction * DriveTrain_Efficiency
-
---Now to compute the linear equivalent
-DriveTrain_PayloadMass = 3.0   --This combines mass with each moment used to compute acceleration
-DriveTrain_MaxAccel_rad= DriveTrain_MaxTorque / DriveTrain_PayloadMass
-DriveTrain_MaxAccel_rps= DriveTrain_MaxAccel_rad / (2.0 * Pi)
-DriveTrain_WheelDiameter = Inches2Meters * FRC2014_wheel_diameter_in
-DriveTrain_MaxAccel_linear = DriveTrain_MaxAccel_rps * (Pi * DriveTrain_WheelDiameter)
-DriveTrain_MaxForce = DriveTrain_MaxAccel_linear * DriveTrain_PayloadMass
-
---extra computations
-DriveTrain_MaxWheelRPS_High = HighGearSpeed / (Pi * DriveTrain_WheelDiameter)  --The fastest RPS of the wheels
-DriveTrain_MaxWheelRPS_Low  = LowGearSpeed  / (Pi * DriveTrain_WheelDiameter)
-DriveTrain_MotorRPS = DriveTrain_MaxWheelRPS_High * DriveTrain_GearReduction
-DriveTrain_MaxAngularVelocity_High = DriveTrain_MaxWheelRPS_High * 2.0 * Pi --In radians
-
-KeyDistance_in=144
---KeyDistance_in=0
-KeyWidth_in=101
-KeyDepth_in=48
-HalfKeyWidth_in=KeyWidth_in/2.0
-
-
 MainRobot = {
 	--Version helps to identify a positive update to lua
 	--version = 1;
@@ -104,126 +71,13 @@ MainRobot = {
 	
 	robot_settings =
 	{
-		key_1 = { x_in=0				, y_in=KeyDistance_in },
-		key_2 = { x_in=-HalfKeyWidth_in	, y_in=KeyDistance_in },
-		key_3 = { x_in= HalfKeyWidth_in	, y_in=KeyDistance_in },
 		ds_display_row=-1,					--This will display the coordinates and heading (may want to leave on)
-		ds_target_vars_row=-1,				--Only used during keying the grid
-		ds_power_velocity_row=-1,			--Only used during keying the grid
-		fire_trigger_delay=0.100,			--Used to wait for a stable rate before engaging the conveyor
-		fire_stay_on_time=0.200,			--Used to prevent ball from get stuck during a fire operation (keep small)
-		
-		grid_corrections =
-		{
-			c11={p=1.7, x=1.0}, c12={p=1.7, x=1.0}, c13={p=1.7, x=1.0},
-			c21={p=1.8, x=1.0}, c22={p=1.8, x=1.0}, c23={p=1.8, x=1.0},
-			c31={p=1.7, x=1.0}, c32={p=1.7, x=1.0}, c33={p=1.7, x=1.0},
-		},
 		
 		auton =
 		{
 			move_forward_ft =0.0,
-			two_shot_scaler =1.4,
-			ramp_left  ={x_in=0, y_in=0 },
-			ramp_right ={x_in=0, y_in=0 },
-			ramp_center={x_in=0, y_in=0 },
-			x_left_arc=1.9,
-			x_right_arc=1.9,
-			--If you put -1.0 for the timeout wait it will wait infinitely (good for initial testing or if we are not tipping ramps)
-			--ball 1 initial wait should be long enough for a good ramp up from zero speed
-			ball_1 ={initial_wait=  2.0, tolerance=75.0, timeout_wait=4.0},
-			--ball 2 initial wait should be long enough to recover from dip and short enough to be active during second ball's deployment
-			ball_2 ={initial_wait=0.500, tolerance=75.0, timeout_wait=4.0}
-			--panic mode incase the wait ball doesn't work... using zero makes it work like before just pure time
-			--ball_1 ={initial_wait=  3.5, tolerance=0.0, timeout_wait=-1.0},
-			--ball_2 ={initial_wait=  3.5, tolerance=0.0, timeout_wait=-1.0}
 		},
 		
-		turret =
-		{
-			is_closed='yes',				--It is closed loop when feedback has been properly calibrated
-			show_pid_dump='no',				--Only turn on if you want to analyze the PID dump (only one at a time, and it must be closed loop)
-			ds_display_row=-1,				--Assign to a row (e.g. 0-4) when trying to calibrate the potentiometer
-			pid=
-			{p=1, i=0, d=0},
-			tolerance=0.001,				--we need high precision
-			encoder_to_wheel_ratio=1.0,		--Used to calibrate encoder to physical turret angles should match readings 
-			voltage_multiply=-1.0,			--May be reversed using -1.0
-			max_speed=10,
-			accel=1.0,						--These are only needed if we bind keys for turret
-			brake=1.0,
-			max_accel_forward=10,			--These are in radians, plan on increasing these as much as possible
-			max_accel_reverse=10,
-			min_range_deg=-180,				--These are probably good to go, but may need to be smaller
-			max_range_deg= 180
-		},
-		pitch =
-		{
-			is_closed='yes',
-			show_pid_dump='no',
-			ds_display_row=-1,
-			pid=
-			{p=1, i=0, d=0},
-			tolerance=0.001,				--we need high precision
-
-			max_speed=10,
-			max_accel_forward=10,			--These are in radians, plan on increasing these as much as possible
-			max_accel_reverse=10,
-			min_range_deg=45-3,				--These should be good to go
-			max_range_deg=70+3
-		},
-		power =
-		{
-			is_closed='yes',
-			show_pid_dump='no',
-			ds_display_row=-1,				--Use this display to determine max speed (try to get a good match)
-			pid=
-			{p=50, i=1, d=25 },
-			latency=0.0,
-			tolerance=10.0,					--we need decent precision (this will depend on ramp up time too)
-			encoder_to_wheel_ratio=0.9215,     --Just use the gearing ratios here
-			voltage_multiply=-1.0,
-			curve_voltage=
-			{t4=3.1199, t3=-4.4664, t2=2.2378, t1=0.1222, c=0},
-
-			length_in=6,					--6 inch diameter (we shouldn't worry about tweaking this just measure it and be done)
-			max_speed=(5000.0/60.0) * Pi2,	--(This is clocked at 5000 rpm) in radians
-			accel=200.0,						--These are only needed if we bind keys for power in meters per second
-			brake=200.0,
-			max_accel_forward=200,			--These are in radians, plan on increasing these as much as possible
-			max_accel_reverse=200,			--The wheel may some time to ramp up
-			min_range=28 * Pi2				--We borrow the min range to represent the min speed
-		},
-		flippers =
-		{
-			is_closed=1,				--Not sure yet about this
-			show_pid_dump='no',
-			ds_display_row=-1,
-			pid=
-			{p=1, i=0, d=0},
-			tolerance=0.01,					--should not matter much
-			
-			max_speed=1.4 * Pi2,			--(Parker gave this one, should be good)
-			accel=10.0,						--We may indeed have a two button solution (match with max accel)
-			brake=10.0,
-			max_accel_forward=10,			--These are in radians, just go with what feels right
-			max_accel_reverse=10,
-			using_range=1,					--Warning Only use range if we have a potentiometer!
-			min_range_deg=-90,				--TODO find out what these are
-			max_range_deg= 90
-		},
-		conveyor =
-		{
-			--Note: there are no encoders here so is_closed is ignored
-			tolerance=0.01,					--we need good precision
-			voltage_multiply=-1.0,			--May be reversed
-			
-			max_speed=28,
-			accel=112,						--These are needed and should be high enough to grip without slip
-			brake=112,
-			max_accel_forward=112,
-			max_accel_reverse=112
-		},
 		low_gear = 
 		{
 			--While it is true we have more torque for low gear, we have to be careful that we do not make this too powerful as it could
@@ -331,16 +185,8 @@ MainRobot = {
 			Joystick_SetCurrentSpeed_2 = {type="joystick_analog", key=1, is_flipped=true, multiplier=1.0, filter=0.1, curve_intensity=0.0},
 			--scaled down to 0.5 to allow fine tuning and a good top acceleration speed (may change with the lua script tweaks)
 			Turret_SetCurrentVelocity = {type="joystick_analog", key=5, is_flipped=false, multiplier=0.5, filter=0.1, curve_intensity=0.0},
-			Ball_Squirt = {type="joystick_button", key=3, on_off=true},
-			Ball_SlowWheel = {type="joystick_button", key=6, on_off=true},
-			Robot_SetPreset1 = {type="joystick_button", key=11, on_off=false},
-			Robot_SetPreset2 = {type="joystick_button", key=9, on_off=false},
-			Robot_SetPreset3 = {type="joystick_button", key=10, on_off=false},
-			Ball_Fire = {type="joystick_button", key=6, on_off=true},
-			PowerWheels_IsRunning = {type="joystick_button", key=5, on_off=true},
-			Ball_GripL = {type="joystick_button", key=1, on_off=true},
-			Ball_GripM = {type="joystick_button", key=2, on_off=true},
-			Ball_GripH = {type="joystick_button", key=4, on_off=true},
+			Robot_SetLowGearOff = {type="joystick_button", key=8, on_off=false},
+			Robot_SetLowGearOn = {type="joystick_button", key=6, on_off=false},
 			POV_Turn =  {type="joystick_analog", key=8, is_flipped=false, multiplier=1.0, filter=0.0, curve_intensity=0.0},
 			Turn_180 = {type="joystick_button", key=7, on_off=false}
 		},
@@ -372,7 +218,7 @@ MainRobot = {
 	UI =
 	{
 		Length=5, Width=5,
-		TextImage="(   )\n|   |\n(   )\n|   |\n(   )"
+		TextImage="(   )\n|   |\n(-+-)\n|   |\n(   )"
 	}
 }
 
--- Trunk/Source/FRC_Source/Robot_Simulation/Robot_Tester/FRC2014_Robot.h
+++ Trunk/Source/FRC_Source/Robot_Simulation/Robot_Tester/FRC2014_Robot.h
@@ -19,60 +19,26 @@ public:
 struct FRC_2014_Robot_Props
 {
 public:
-	//typedef Framework::Base::Vec2d Vec2D;
-	typedef osg::Vec2d Vec2D;
-	
-	Vec2D PresetPositions[3];
-	Vec2D KeyGrid[3][3];
-	struct DeliveryCorrectionFields
-	{
-		double PowerCorrection;
-		double YawCorrection;
-	};
-	DeliveryCorrectionFields KeyCorrections[3][3];
-	double FireTriggerDelay;		//Time for stable signal before triggering the fire
-	double FireButtonStayOn_Time;   //Time to stay on before stopping the conveyors
-	size_t Coordinates_DiplayRow;
-	size_t TargetVars_DisplayRow;
-	size_t PowerVelocity_DisplayRow;
-
 	struct Autonomous_Properties
 	{
-		Vec2D RampLeft_ErrorCorrection_Offset;
-		Vec2D RampRight_ErrorCorrection_Offset;
-		Vec2D RampCenter_ErrorCorrection_Offset;
-		double XLeftArc,XRightArc;
-		struct WaitForBall_Info
-		{
-			double InitialWait;
-			double TimeOutWait;			//If -1 then it is infinite and will not multi task a wait time (great for testing)
-			double ToleranceThreshold;  //If zero then only the initial wait is used for each ball (or not using the wait for ball feature)
-		} FirstBall_Wait,SecondBall_Wait; //We'll want to tweak the second ball a bit differently
 		double MoveForward;				//Optional to move forward to use less power to shoot
-		double TwoShotScaler;			//Scaler used for two point shots
 	} Autonomous_Props;
 };
 
 class FRC_2014_Robot_Properties : public Tank_Robot_Properties
 {
 	public:
-		//typedef Framework::Base::Vec2d Vec2D;
-		typedef osg::Vec2d Vec2D;
-
 		FRC_2014_Robot_Properties();
 		virtual void LoadFromScript(GG_Framework::Logic::Scripting::Script& script);
 
 		const Rotary_Properties &GetTurretProps() const {return m_TurretProps;}
 		const Rotary_Properties &GetPitchRampProps() const {return m_PitchRampProps;}
-		const Rotary_Properties &GetPowerWheelProps() const {return m_PowerWheelProps;}
-		const Rotary_Properties &GetConveyorProps() const {return m_ConveyorProps;}
-		const Rotary_Properties &GetFlipperProps() const {return m_FlipperProps;}
 		const Tank_Robot_Properties &GetLowGearProps() const {return m_LowGearProps;}
 		const FRC_2014_Robot_Props &GetFRC2014RobotProps() const {return m_FRC2014RobotProps;}
 		const LUA_Controls_Properties &Get_RobotControls() const {return m_RobotControls;}
 	private:
 		//typedef Tank_Robot_Properties __super;
-		Rotary_Properties m_TurretProps,m_PitchRampProps,m_PowerWheelProps,m_ConveyorProps,m_FlipperProps;
+		Rotary_Properties m_TurretProps,m_PitchRampProps;
 		Tank_Robot_Properties m_LowGearProps;
 		FRC_2014_Robot_Props m_FRC2014RobotProps;
 
@@ -92,38 +58,13 @@ class FRC_2014_Robot : public Tank_Robot
 		{
 			eTurret,
 			ePitchRamp,
-			ePowerWheels,
-			eLowerConveyor,
-			eMiddleConveyor,
-			eFireConveyor,
-			eFlippers
-		};
-
-		enum BoolSensorDevices
-		{
-			eLowerConveyor_Sensor,
-			eMiddleConveyor_Sensor,
-			eFireConveyor_Sensor
 		};
 
 		enum SolenoidDevices
 		{
 			eUseLowGear,		//If the OpenSolenoid() is called with true then it should be in low gear; otherwise high gear
-			eFlipperDown,		//If true flipper is down
-			eUseBreakDrive,		//OpenSolenoid() is called with true then its in break mode (default is coast) This is really a digital output
-			eRampDeployment
 		};
 
-		//typedef Framework::Base::Vec2d Vec2D;
-		typedef osg::Vec2d Vec2D;
-
-		enum Targets
-		{
-			eCenterHighGoal,
-			eLeftGoal,
-			eRightGoal,
-			eDefensiveKey
-		};
 		FRC_2014_Robot(const char EntityName[],FRC_2014_Control_Interface *robot_control,bool IsAutonomous=false);
 		IEvent::HandlerList ehl;
 		virtual void Initialize(Entity2D::EventMap& em, const Entity_Properties *props=NULL);
@@ -169,125 +110,20 @@ class FRC_2014_Robot : public Tank_Robot
 				FRC_2014_Robot * const m_pParent;
 		};
 
-		class PowerWheels : public Rotary_Velocity_Control
-		{
-			public:
-				PowerWheels(FRC_2014_Robot *pParent,Rotary_Control_Interface *robot_control);
-				IEvent::HandlerList ehl;
-				virtual void BindAdditionalEventControls(bool Bind);
-				virtual void ResetPos();
-			protected:
-				//typedef Rotary_Velocity_Control __super;
-				//events are a bit picky on what to subscribe so we'll just wrap from here
-				void SetRequestedVelocity_FromNormalized(double Velocity);
-				void SetEncoderSafety(bool DisableFeedback) {__super::SetEncoderSafety(DisableFeedback);}
-				void SetIsRunning(bool IsRunning) {m_IsRunning=IsRunning;}
-				virtual void TimeChange(double dTime_s);
-			private:
-				FRC_2014_Robot * const m_pParent;
-				double m_ManualVelocity;
-				bool m_IsRunning;
-		};
-
-		class BallConveyorSystem
-		{
-			private:
-				FRC_2014_Robot * const m_pParent;
-				Rotary_Velocity_Control m_LowerConveyor,m_MiddleConveyor,m_FireConveyor;
-				double m_FireDelayTrigger_Time; //Time counter of the value remaining in the on-to-delay state
-				double m_FireStayOn_Time;  //Time counter of the value remaining in the on state
-				bool m_FireDelayTriggerOn; //A valve mechanism that must meet time requirement to disable the delay
-				bool m_FireStayOn;		   //A valve mechanism that must time out to turn off
-				union ControlSignals
-				{
-					struct ControlSignals_rw
-					{
-						unsigned char Grip   : 1;
-						unsigned char Squirt : 1;
-						unsigned char Fire   : 1;
-						unsigned char GripL  : 1;	//Manual grip low, medium, and high
-						unsigned char GripM  : 1;
-						unsigned char GripH  : 1;
-					} bits;
-					unsigned char raw;
-				} m_ControlSignals;
-			public:
-				BallConveyorSystem(FRC_2014_Robot *pParent,Rotary_Control_Interface *robot_control);
-				void Initialize(GG_Framework::Base::EventMap& em,const Entity1D_Properties *props=NULL);
-				bool GetIsFireRequested() const {return m_ControlSignals.bits.Fire==1;}
-				IEvent::HandlerList ehl;
-
-				void ResetPos();
-				//Intercept the time change to send out voltage
-				void TimeChange(double dTime_s);
-				void BindAdditionalEventControls(bool Bind);
-				//Expose for goals
-				void Fire(bool on) {m_ControlSignals.bits.Fire=on;}
-				void Squirt(bool on) {m_ControlSignals.bits.Squirt=on;}
-			protected:
-				//Using meaningful terms to assert the correct direction at this level
-				void Grip(bool on) {m_ControlSignals.bits.Grip=on;}
-				void GripL(bool on) {m_ControlSignals.bits.GripL=on;}
-				void GripM(bool on) {m_ControlSignals.bits.GripM=on;}
-				void GripH(bool on) {m_ControlSignals.bits.GripH=on;}
-
-				void SetRequestedVelocity_FromNormalized(double Velocity);
-		};
-
-		class Flippers : public Rotary_Position_Control
-		{
-			private:
-				//typedef Rotary_Position_Control __super;
-				FRC_2014_Robot * const m_pParent;
-				bool m_Advance,m_Retract;
-			public:
-				Flippers(FRC_2014_Robot *pParent,Rotary_Control_Interface *robot_control);
-				IEvent::HandlerList ehl;
-				virtual void BindAdditionalEventControls(bool Bind);
-			protected:
-
-				void Advance(bool on) {m_Advance=on;}
-				void Retract(bool on) {m_Retract=on;}
-
-				//typedef Rotary_Position_Control __super;
-				//events are a bit picky on what to subscribe so we'll just wrap from here
-				void SetRequestedVelocity_FromNormalized(double Velocity) {__super::SetRequestedVelocity_FromNormalized(Velocity);}
-				void SetIntendedPosition(double Position);
-
-				void SetPotentiometerSafety(bool DisableFeedback) {__super::SetPotentiometerSafety(DisableFeedback);}
-				virtual void TimeChange(double dTime_s);
-		};
 
 	public: //Autonomous public access (wind river has problems with friend technique)
-		BallConveyorSystem &GetBallConveyorSystem();
-		PowerWheels &GetPowerWheels();
-		void SetPresetPosition(size_t index,bool IgnoreOrientation=false);
-		void Set_Auton_PresetPosition(size_t index);
-		void SetTarget(Targets target);
 		const FRC_2014_Robot_Properties &GetRobotProps() const;
-		void SetFlipperPneumatic(bool on) {m_RobotControl->OpenSolenoid(eFlipperDown,on);}
 	protected:
 		virtual void BindAdditionalEventControls(bool Bind);
 		virtual void BindAdditionalUIControls(bool Bind, void *joy, void *key);
 	private:
-		void ApplyErrorCorrection();
 		//typedef  Tank_Robot __super;
 		FRC_2014_Control_Interface * const m_RobotControl;
 		Turret m_Turret;
 		PitchRamp m_PitchRamp;
-		PowerWheels m_PowerWheels;
-		BallConveyorSystem m_BallConveyorSystem;
-		Flippers m_Flippers;
 		FRC_2014_Robot_Properties m_RobotProps;  //saves a copy of all the properties
-		Targets m_Target;		//This allows us to change our target
 		Vec2D m_DefensiveKeyPosition;
 
-		//This is adjusted depending on location for correct bank-shot angle trajectory, note: the coordinate system is based where 0,0 is the 
-		//middle of the game playing field
-		Vec2D m_TargetOffset;  //2d top view x,y of the target
-		//This is adjusted depending on doing a bank shot or swishing 
-		double m_TargetHeight;  //1d z height (front view) of the target
-		//cached during robot time change and applied to other systems when targeting is true
 		double m_PitchAngle,m_LinearVelocity,m_HangTime;
 		double m_YawErrorCorrection,m_PowerErrorCorrection;
 		double m_DefensiveKeyNormalizedDistance;
@@ -296,29 +132,11 @@ class FRC_2014_Robot : public Tank_Robot
 		bool m_DisableTurretTargetingValue;
 		bool m_POVSetValve;
 
-		bool m_IsTargeting;
-		void IsTargeting(bool on) {m_IsTargeting=on;}
-		void SetTargetingOn() {IsTargeting(true);}
-		void SetTargetingOff() {IsTargeting(false);}
-		void SetTurretTargetingOff(bool on) {m_DisableTurretTargetingValue=on;}
-		void SetTargetingValue(double Value);
-
 		bool m_SetLowGear;
 		void SetLowGear(bool on);
 		void SetLowGearOn() {SetLowGear(true);}
 		void SetLowGearOff() {SetLowGear(false);}
 		void SetLowGearValue(double Value);
-		
-		void SetPreset1() {SetPresetPosition(0);}
-		void SetPreset2() {SetPresetPosition(1);}
-		void SetPreset3() {SetPresetPosition(2);}
-		void SetPresetPOV (double value);
-
-		void SetDefensiveKeyPosition(double NormalizedDistance) {m_DefensiveKeyNormalizedDistance=NormalizedDistance;}
-		void SetDefensiveKeyOn();
-		void SetDefensiveKeyOff() {m_Target=eCenterHighGoal;}
-
-		void Robot_SetCreepMode(bool on);
 };
 
 class FRC_2014_Goals
@@ -377,7 +195,6 @@ class FRC_2014_Robot_Control : public FRC_2014_Control_Interface
 		const FRC_2014_Robot_Properties &GetRobotProps() const {return m_RobotProps;}
 	protected: //from Robot_Control_Interface
 		virtual void UpdateVoltage(size_t index,double Voltage);
-		virtual bool GetBoolSensorState(size_t index);
 		virtual void CloseSolenoid(size_t index,bool Close) {OpenSolenoid(index,!Close);}
 		virtual void OpenSolenoid(size_t index,bool Open);
 	protected: //from Tank_Drive_Control_Interface
@@ -397,11 +214,6 @@ class FRC_2014_Robot_Control : public FRC_2014_Control_Interface
 		//Note: This is only for Robot Tester
 		virtual void BindAdditionalEventControls(bool Bind,GG_Framework::Base::EventMap *em,IEvent::HandlerList &ehl);
 
-		void TriggerLower(bool on) {m_LowerSensor=on;}
-		void TriggerMiddle(bool on) {m_MiddleSensor=on;}
-		void TriggerFire(bool on) {m_FireSensor=on;}
-		void SlowWheel(bool on) {m_SlowWheel=on;}
-
 	protected:
 		FRC_2014_Robot_Properties m_RobotProps;  //saves a copy of all the properties
 		Tank_Robot_Control m_TankRobotControl;
@@ -412,79 +224,8 @@ class FRC_2014_Robot_Control : public FRC_2014_Control_Interface
 		//cache voltage values for display
 		double m_TurretVoltage,m_PitchRampVoltage,m_PowerWheelVoltage,m_FlipperVoltage;
 		double m_LowerConveyorVoltage,m_MiddleConveyorVoltage,m_FireConveyorVoltage;
-		bool m_LowerSensor,m_MiddleSensor,m_FireSensor;
-		bool m_SlowWheel;
 };
 
-class FRC_2014_Turret_UI
-{
-	public:
-		typedef osg::Vec2d Vec2D;
-
-		struct Turret_Properties
-		{
-			double YOffset;
-		};
-
-		FRC_2014_Turret_UI(FRC_2014_Robot_Control *robot_control) : m_RobotControl(robot_control) {}
-		virtual void Initialize(Entity2D::EventMap& em, const Turret_Properties *props=NULL);
-
-		virtual void UI_Init(Actor_Text *parent);
-		virtual void update(osg::NodeVisitor *nv, osg::Drawable *draw,const osg::Vec3 &parent_pos,double Heading);
-		virtual void Text_SizeToUse(double SizeToUse);
-
-		virtual void UpdateScene (osg::Geode *geode, bool AddOrRemove);
-	private:
-		FRC_2014_Robot_Control * const m_RobotControl;
-		Actor_Text *m_UIParent;
-		Turret_Properties m_props;
-		osg::ref_ptr<osgText::Text> m_Turret; 
-};
-
-class FRC_2014_Power_Wheel_UI : public Side_Wheel_UI
-{
-	public:
-		FRC_2014_Power_Wheel_UI(FRC_2014_Robot_Control *robot_control) : m_RobotControl(robot_control) {}
-		//Client code can manage the properties
-		virtual void Initialize(Entity2D::EventMap& em, const Wheel_Properties *props=NULL);
-		virtual void TimeChange(double dTime_s);
-	private:
-		FRC_2014_Robot_Control * const m_RobotControl;
-		double m_PowerWheelMaxSpeed;  //cache to avoid all the hoops of getting it (its constant)
-};
-
-class FRC_2014_Lower_Conveyor_UI : public Side_Wheel_UI
-{
-	public:
-		FRC_2014_Lower_Conveyor_UI(FRC_2014_Robot_Control *robot_control) : m_RobotControl(robot_control) {}
-		//Client code can manage the properties
-		virtual void Initialize(Entity2D::EventMap& em, const Wheel_Properties *props=NULL);
-		virtual void TimeChange(double dTime_s);
-	private:
-		FRC_2014_Robot_Control * const m_RobotControl;
-};
-
-class FRC_2014_Middle_Conveyor_UI : public Side_Wheel_UI
-{
-	public:
-		FRC_2014_Middle_Conveyor_UI(FRC_2014_Robot_Control *robot_control) : m_RobotControl(robot_control) {}
-		//Client code can manage the properties
-		virtual void Initialize(Entity2D::EventMap& em, const Wheel_Properties *props=NULL);
-		virtual void TimeChange(double dTime_s);
-	private:
-		FRC_2014_Robot_Control * const m_RobotControl;
-};
-
-class FRC_2014_Fire_Conveyor_UI : public Side_Wheel_UI
-{
-	public:
-		FRC_2014_Fire_Conveyor_UI(FRC_2014_Robot_Control *robot_control) : m_RobotControl(robot_control) {}
-		//Client code can manage the properties
-		virtual void Initialize(Entity2D::EventMap& em, const Wheel_Properties *props=NULL);
-		virtual void TimeChange(double dTime_s);
-	private:
-		FRC_2014_Robot_Control * const m_RobotControl;
-};
 
 ///This is only for the simulation where we need not have client code instantiate a Robot_Control
 class FRC_2014_Robot_UI : public FRC_2014_Robot, public FRC_2014_Robot_Control
@@ -503,9 +244,4 @@ class FRC_2014_Robot_UI : public FRC_2014_Robot, public FRC_2014_Robot_Control
 
 	private:
 		Tank_Robot_UI m_TankUI;
-		FRC_2014_Turret_UI m_TurretUI;
-		FRC_2014_Power_Wheel_UI m_PowerWheelUI;
-		FRC_2014_Lower_Conveyor_UI m_LowerConveyor;
-		FRC_2014_Middle_Conveyor_UI m_MiddleConveyor;
-		FRC_2014_Fire_Conveyor_UI m_FireConveyor;
 };
