93ebd83f525aa0b06517f6dea4ac954a221f770b
james <james@e2c3bcc0-b32a-0410-840c-db224dcf21cb>
2014-02-12 16:01:19 +0000

    Condensed LUT initialization to generic functions for each type

--- Trunk/Source/FRC_Source/Robot_Simulation/Common/Robot_Control_Common.cpp
+++ Trunk/Source/FRC_Source/Robot_Simulation/Common/Robot_Control_Common.cpp
@@ -126,6 +126,61 @@ RobotControlCommon::~RobotControlCommon()
 
 }
 
+template <class T>
+__inline void Initialize_1C_LUT(const Control_Assignment_Properties::Controls_1C &control_props,std::vector<T *> &constrols,
+								RobotControlCommon::Controls_LUT &control_LUT,RobotControlCommon *instance,size_t (RobotControlCommon::*delegate)(const char *name) const)
+{
+	typedef Control_Assignment_Properties::Controls_1C Controls_1C;
+	typedef Control_Assignment_Properties::Control_Element_1C Control_Element_1C;
+	for (size_t i=0;i<control_props.size();i++)
+	{
+		const Control_Element_1C &element=control_props[i];
+		//create the new Control
+		#ifdef Robot_TesterCode
+		T *NewElement=new T(element.Module,element.Channel,element.name.c_str());  //adding name for UI
+		#else
+		T *NewElement=new T(element.Module,element.Channel);
+		#endif
+		const size_t PopulationIndex=constrols.size();  //get the ordinal value before we add it
+		constrols.push_back(NewElement);  //add it to our list of victors
+		//Now to work out the new LUT
+		size_t enumIndex=(instance->*delegate)(element.name.c_str());
+		//our LUT is the EnumIndex position set to the value of i... make sure we have the slots created
+		assert(enumIndex<10);  //sanity check we have a limit to how many victors we have
+		while(control_LUT.size()<=enumIndex)
+			control_LUT.push_back(-1);  //fill with -1 as a way to indicate nothing is located for that slot
+		control_LUT[enumIndex]=i;
+	}
+}
+
+template <class T>
+__inline void Initialize_2C_LUT(const Control_Assignment_Properties::Controls_2C &control_props,std::vector<T *> &constrols,
+								RobotControlCommon::Controls_LUT &control_LUT,RobotControlCommon *instance,size_t (RobotControlCommon::*delegate)(const char *name) const)
+{
+	typedef Control_Assignment_Properties::Controls_2C Controls_2C;
+	typedef Control_Assignment_Properties::Control_Element_2C Control_Element_2C;
+	for (size_t i=0;i<control_props.size();i++)
+	{
+		const Control_Element_2C &element=control_props[i];
+		//create the new Control
+		#ifdef Robot_TesterCode
+		T *NewElement=new T(element.Module,element.ForwardChannel,element.ReverseChannel,element.name.c_str());
+		#else
+		T *NewElement=new T(element.Module,element.ForwardChannel,element.ReverseChannel);
+		#endif
+		const size_t PopulationIndex=constrols.size();  //get the ordinal value before we add it
+		constrols.push_back(NewElement);  //add it to our list of victors
+		//Now to work out the new LUT
+		size_t enumIndex=(instance->*delegate)(element.name.c_str());
+		//our LUT is the EnumIndex position set to the value of i... make sure we have the slots created
+		assert(enumIndex<10);  //sanity check we have a limit to how many victors we have
+		while(control_LUT.size()<=enumIndex)
+			control_LUT.push_back(-1);  //fill with -1 as a way to indicate nothing is located for that slot
+		control_LUT[enumIndex]=i;
+	}
+}
+
+
 void RobotControlCommon::RobotControlCommon_Initialize(const Control_Assignment_Properties &props)
 {
 	m_Props=props;
@@ -134,76 +189,12 @@ void RobotControlCommon::RobotControlCommon_Initialize(const Control_Assignment_
 	typedef Control_Assignment_Properties::Controls_2C Controls_2C;
 	typedef Control_Assignment_Properties::Control_Element_2C Control_Element_2C;
 	//create control elements and their LUT's
-	{
-		//victors
-		const Controls_1C &control_props=props.GetVictors();
-		for (size_t i=0;i<control_props.size();i++)
-		{
-			const Control_Element_1C &element=control_props[i];
-			//create the new Victor
-			#ifdef Robot_TesterCode
-			Victor *NewVictor=new Victor(element.Module,element.Channel,element.name.c_str());  //adding name for UI
-			#else
-			Victor *NewVictor=new Victor(element.Module,element.Channel);
-			#endif
-			const size_t PopulationIndex=m_Victors.size();  //get the ordinal value before we add it
-			m_Victors.push_back(NewVictor);  //add it to our list of victors
-			//Now to work out the new LUT
-			size_t enumIndex=RobotControlCommon_Get_Victor_EnumValue(element.name.c_str());
-			//our LUT is the EnumIndex position set to the value of i... make sure we have the slots created
-			assert(enumIndex<10);  //sanity check we have a limit to how many victors we have
-			while(m_VictorLUT.size()<=enumIndex)
-				m_VictorLUT.push_back(-1);  //fill with -1 as a way to indicate nothing is located for that slot
-			m_VictorLUT[enumIndex]=i;
-		}
-	}
-	{
-		//double solenoids
-		const Controls_2C &control_props=props.GetDoubleSolenoids();
-		for (size_t i=0;i<control_props.size();i++)
-		{
-			const Control_Element_2C &element=control_props[i];
-			//create the new Control
-			#ifdef Robot_TesterCode
-			DoubleSolenoid *NewElement=new DoubleSolenoid(element.Module,element.ForwardChannel,element.ReverseChannel,element.name.c_str());
-			#else
-			DoubleSolenoid *NewElement=new DoubleSolenoid(element.Module,element.ForwardChannel,element.ReverseChannel);
-			#endif
-			const size_t PopulationIndex=m_DoubleSolenoids.size();  //get the ordinal value before we add it
-			m_DoubleSolenoids.push_back(NewElement);  //add it to our list of victors
-			//Now to work out the new LUT
-			size_t enumIndex=RobotControlCommon_Get_DoubleSolenoid_EnumValue(element.name.c_str());
-			//our LUT is the EnumIndex position set to the value of i... make sure we have the slots created
-			assert(enumIndex<10);  //sanity check we have a limit to how many victors we have
-			while(m_DoubleSolenoidLUT.size()<=enumIndex)
-				m_DoubleSolenoidLUT.push_back(-1);  //fill with -1 as a way to indicate nothing is located for that slot
-			m_DoubleSolenoidLUT[enumIndex]=i;
-		}
-	}
-	{
-		//digital inputs
-		const Controls_1C &control_props=props.GetDigitalInputs();
-		for (size_t i=0;i<control_props.size();i++)
-		{
-			const Control_Element_1C &element=control_props[i];
-			//create the new Control
-			#ifdef Robot_TesterCode
-			DigitalInput *NewElement=new DigitalInput(element.Module,element.Channel,element.name.c_str());  //adding name for UI
-			#else
-			DigitalInput *NewElement=new DigitalInput(element.Module,element.Channel);
-			#endif
-			const size_t PopulationIndex=m_DigitalInputs.size();  //get the ordinal value before we add it
-			m_DigitalInputs.push_back(NewElement);  //add it to our list of victors
-			//Now to work out the new LUT
-			size_t enumIndex=RobotControlCommon_Get_DigitalInput_EnumValue(element.name.c_str());
-			//our LUT is the EnumIndex position set to the value of i... make sure we have the slots created
-			assert(enumIndex<10);  //sanity check we have a limit to how many victors we have
-			while(m_DigitalInputLUT.size()<=enumIndex)
-				m_DigitalInputLUT.push_back(-1);  //fill with -1 as a way to indicate nothing is located for that slot
-			m_DigitalInputLUT[enumIndex]=i;
-		}
-	}
-
+	//victors
+	Initialize_1C_LUT<Victor>(props.GetVictors(),m_Victors,m_VictorLUT,this,&RobotControlCommon::RobotControlCommon_Get_Victor_EnumValue);
+	//double solenoids
+	Initialize_2C_LUT<DoubleSolenoid>(props.GetDoubleSolenoids(),m_DoubleSolenoids,m_DoubleSolenoidLUT,this,&RobotControlCommon::RobotControlCommon_Get_DoubleSolenoid_EnumValue);
+	//digital inputs
+	Initialize_1C_LUT<DigitalInput>(props.GetDigitalInputs(),m_DigitalInputs,m_DigitalInputLUT,this,&RobotControlCommon::RobotControlCommon_Get_DigitalInput_EnumValue);
 }
 
 #ifdef Robot_TesterCode
