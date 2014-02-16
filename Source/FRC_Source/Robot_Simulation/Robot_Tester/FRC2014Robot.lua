c30962a6b46782841d29fb1f6ec1c53de3668bcf
james <james@e2c3bcc0-b32a-0410-840c-db224dcf21cb>
2014-02-16 22:36:47 +0000

    Fixed several bugs with ability to drive to waypoint... seems to finally be correct!

--- Source/FRC_Source/Robot_Simulation/Robot_Tester/FRC2014Robot.lua
+++ Source/FRC_Source/Robot_Simulation/Robot_Tester/FRC2014Robot.lua
@@ -380,6 +380,7 @@ MainRobot = {
 			--PitchRamp_SetCurrentVelocity = {type="joystick_analog", key=4, is_flipped=true, multiplier=1.0, filter=0.1, curve_intensity=2.0},
 			Robot_SetLowGearOff = {type="joystick_button", key=6, on_off=false},
 			Robot_SetLowGearOn = {type="joystick_button", key=5, on_off=false},
+			Robot_TestWaypoint={type="joystick_button", key=3, on_off=true},
 			
 			Winch_SetChipShot = {type="joystick_button", key=4, on_off=false},
 			Winch_SetGoalShot = {type="joystick_button", key=2, on_off=false},
