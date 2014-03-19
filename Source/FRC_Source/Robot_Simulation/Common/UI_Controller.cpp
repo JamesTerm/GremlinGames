94ef26f02dedbd8b848034085514b2aea26fe84e
james <james@e2c3bcc0-b32a-0410-840c-db224dcf21cb>
2014-03-19 20:23:59 +0000

    Fixed broken circle issue

--- Source/FRC_Source/Robot_Simulation/Common/UI_Controller.cpp
+++ Source/FRC_Source/Robot_Simulation/Common/UI_Controller.cpp
@@ -164,7 +164,17 @@ void UI_Controller::FieldCentricDrive::TimeChange(double dTime_s)
 		const double lookDir_radians=NormalizeRotation2(theta + m_HeadingLock);
 		//evaluate delta offset to see if we want to use the reverse absolute position
 		Ship_2D &m_ship=*m_pParent->m_ship;
-		const double OrientationDelta=lookDir_radians-m_ship.GetAtt_r();
+		double OrientationDelta;
+		{
+			using namespace std;
+			const double current_Att_ABS=fabs(m_ship.GetAtt_r());
+			const double intended_Att_ABS=fabs(lookDir_radians);
+			const double Begin=min(current_Att_ABS,intended_Att_ABS);
+			const double End=max(current_Att_ABS,intended_Att_ABS);
+			const double NormalDelta=End-Begin;  //normal range  -------BxxxxxE-------
+			const double InvertedDelta=End+(Pi-Begin); //inverted range  xxxxE---------Bxxxx
+			OrientationDelta=min(NormalDelta,InvertedDelta);
+		}
 		double orientation_to_use=lookDir_radians;
 		double NormalizedVelocity=cos(fabs(OrientationDelta))*magnitude;
 		if (fabs(OrientationDelta)>PI_2)
