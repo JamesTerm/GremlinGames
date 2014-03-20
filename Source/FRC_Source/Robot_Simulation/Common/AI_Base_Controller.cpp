aa3d473be9b9435af5b1d5fbf7c2f8ddca60cfbc
james <james@e2c3bcc0-b32a-0410-840c-db224dcf21cb>
2014-03-20 03:22:37 +0000

    More bug fixes within the orientation delta computation... also properly flipping of orientation is being used.  Heading lock needed to be normalized.  Air flo now set up to use field centric controls

--- Source/FRC_Source/Robot_Simulation/Common/AI_Base_Controller.cpp
+++ Source/FRC_Source/Robot_Simulation/Common/AI_Base_Controller.cpp
@@ -524,17 +524,19 @@ void AI_Base_Controller::DriveToLocation(Vec2d TrajectoryPoint,Vec2d PositionPoi
 			double OrientationDelta;
 			{
 				using namespace std;
-				const double current_Att_ABS=fabs(m_ship.GetAtt_r());
-				const double intended_Att_ABS=fabs(lookDir_radians);
+				const double current_Att_ABS=m_ship.GetAtt_r() + Pi;
+				const double intended_Att_ABS=lookDir_radians + Pi;
 				const double Begin=min(current_Att_ABS,intended_Att_ABS);
 				const double End=max(current_Att_ABS,intended_Att_ABS);
-				const double NormalDelta=End-Begin;  //normal range  -------BxxxxxE-------
-				const double InvertedDelta=End+(Pi-Begin); //inverted range  xxxxE---------Bxxxx
+				const double NormalDelta=End-Begin;			//normal range  -------BxxxxxE-------
+				const double InvertedDelta=Begin+(Pi2-End);	//inverted range  xxxxB---------Exxxx
 				OrientationDelta=min(NormalDelta,InvertedDelta);
 			}
 			double orientation_to_use=lookDir_radians;
-			if (fabs(OrientationDelta)>PI_2)
-				orientation_to_use=NormalizeRotation_HalfPi(lookDir_radians);
+			//Note the condition to flip has a little bit of extra tolerance to avoid a excessive flipping back and forth
+			//We simply multiply the half pi by a scalar
+			if (fabs(OrientationDelta)>(PI_2*1.2))
+				orientation_to_use=NormalizeRotation2(lookDir_radians + Pi);
 			m_ship.SetIntendedOrientation(orientation_to_use);
 		}
 		#endif
