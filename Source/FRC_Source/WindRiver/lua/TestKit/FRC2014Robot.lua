0c64f90f95f5c266f5a0d82e195d33c61f8ca696
james <james@e2c3bcc0-b32a-0410-840c-db224dcf21cb>
2014-03-29 22:27:48 +0000

    Merged with 1.6 on practice

--- Source/FRC_Source/WindRiver/lua/TestKit/FRC2014Robot.lua
+++ Source/FRC_Source/WindRiver/lua/TestKit/FRC2014Robot.lua
@@ -26,15 +26,20 @@ g_wheel_diameter_in=4   --This will determine the correct distance try to make a
 WheelBase_Width_In=26.5	  --The wheel base will determine the turn rate, must be as accurate as possible!
 WheelBase_Length_In=10  --was 9.625
 WheelTurningDiameter_In= ( (WheelBase_Width_In * WheelBase_Width_In) + (WheelBase_Length_In * WheelBase_Length_In) ) ^ 0.5
-HighGearSpeed = (733.14 / 60.0) * Pi * g_wheel_diameter_in * Inches2Meters  --RPM's from Parker
-LowGearSpeed  = (167.06 / 60.0) * Pi * g_wheel_diameter_in * Inches2Meters
+HighGearSpeed = (873.53 / 60.0) * Pi * g_wheel_diameter_in * Inches2Meters  * 0.85 --RPM's from Parker
+LowGearSpeed  = (403.92 / 60.0) * Pi * g_wheel_diameter_in * Inches2Meters  * 0.9
 Drive_MaxAccel=5
 skid=math.cos(math.atan2(WheelBase_Length_In,WheelBase_Width_In))
 gMaxTorqueYaw = (2 * Drive_MaxAccel * Meters2Inches / WheelTurningDiameter_In) * skid
 
 MainRobot = {
 	--Version helps to identify a positive update to lua
-	version = 1;
+	--Version 1.4 was practice bot before Dallas
+	--Version 1.3 was competition bot during Dallas, and merged into practice lua
+	--Version 1.5 is 1.3 merge plus preliminary draft with corrected gear ratios, low gear fixes in torque and gain assist
+	--				as well as added tank strafe axis assignments
+	--Version 1.6 Drive gear rates and encoders calibrated
+	version = 1.6;
 	control_assignments =
 	{
 		--by default module is 1, so only really need it for 2
@@ -83,9 +88,10 @@ MainRobot = {
 	MaxAccelLeft = 20, MaxAccelRight = 20, 
 	MaxAccelForward = Drive_MaxAccel, MaxAccelReverse = Drive_MaxAccel, 
 	MaxAccelForward_High = Drive_MaxAccel * 2, MaxAccelReverse_High = Drive_MaxAccel * 2, 
-	MaxTorqueYaw =  gMaxTorqueYaw,
+	MaxTorqueYaw =  gMaxTorqueYaw * 0.78,
 	MaxTorqueYaw_High = gMaxTorqueYaw * 5,
-	rotate_to_scale = 1.0, rotate_to_scale_high = 1.0,
+	MaxTorqueYaw_SetPoint = gMaxTorqueYaw * 2,
+	MaxTorqueYaw_SetPoint_High = gMaxTorqueYaw * 10,
 	rotation_tolerance=Deg2Rad * 2,
 	rotation_distance_scalar=1.0,
 
@@ -117,8 +123,8 @@ MainRobot = {
 		drive_to_scale=0.50,				--For 4 to 10 50% gives a 5 inch tolerance
 		left_max_offset=0.0 , right_max_offset=0.0,   --Ensure both tread top speeds are aligned
 		--This is obtainer from encoder RPM's of 1069.2 and Wheel RPM's 427.68 (both high and low have same ratio)
-		encoder_to_wheel_ratio=0.4,			--example if encoder spins at 1069.2 multiply by this to get 427.68 (for the wheel rpm)
-		voltage_multiply=1.0,				--May be reversed using -1.0
+		encoder_to_wheel_ratio=0.5,			--example if encoder spins at 1069.2 multiply by this to get 427.68 (for the wheel rpm)
+		voltage_multiply=-1.0,				--May be reversed using -1.0
 		--curve_voltage=
 		--{t4=3.1199, t3=-4.4664, t2=2.2378, t1=0.1222, c=0},
 		force_voltage=
@@ -126,7 +132,7 @@ MainRobot = {
 		reverse_steering='no',
 		 left_encoder_reversed='no',
 		right_encoder_reversed='no',
-		inv_max_accel = 1/15.0,  --solved empiracally
+		inv_max_accel = 1/30.0,  --solved empiracally
 		forward_deadzone_left  = 0.02,
 		forward_deadzone_right = 0.02,
 		reverse_deadzone_left  = 0.02,
@@ -135,7 +141,7 @@ MainRobot = {
 		{
 			wheel_mass=1.5,
 			cof_efficiency=1.0,
-			gear_reduction=5310.0/733.14,
+			gear_reduction=5310.0/873.53,
 			torque_on_wheel_radius=Inches2Meters * 1,
 			drive_wheel_radius=Inches2Meters * 2,
 			number_of_motors=1,
@@ -175,21 +181,31 @@ MainRobot = {
 
 		auton =
 		{
-			move_forward_ft =0.0,
+			first_move_ft=2,
+			second_move_ft=4,
+			support_hotspot='y',
+			land_on_ball_roller_speed=0.75;
+			land_on_ball_roller_time=0.5,
+			second_ball_roller_time=1,
+			roller_drive_speed=0.75,
+			third_ball_angle_deg=45,
+			-- (x / cos(theta)) should be same distance back, use negative to come backwards
+			third_ball_distance_ft=-(2/math.cos(45 * Deg2Rad)),
+			show_auton_variables='y'
 		},
 		
 		winch =
 		{
 			--is_closed=0,
 			starting_position_deg=0,
-			show_pid_dump='y',
+			show_pid_dump='n',
 			ds_display_row=-1,
 			use_pid_up_only='y',
 			pid_up=
 			{p=100, i=0, d=0},
 			tolerance=0.15,
 			tolerance_count=20,
-			voltage_multiply=1.0,			--May be reversed
+			voltage_multiply=-1.0,			--May be reversed
 			encoder_to_wheel_ratio=Catapult_PotentiometerToMotorRatio,
 			--curve_voltage=
 			--{t4=3.1199, t3=-4.4664, t2=2.2378, t1=0.1222, c=0},
@@ -303,8 +319,9 @@ MainRobot = {
 			--While it is true we have more torque for low gear, we have to be careful that we do not make this too powerful as it could
 			--cause slipping if driver "high sticks" to start or stop quickly.
 			MaxAccelLeft = 10, MaxAccelRight = 10, MaxAccelForward = 10 * 2, MaxAccelReverse = 10 * 2, 
-			MaxTorqueYaw = 25 * 2, 
-			
+			MaxTorqueYaw = 25 * 2,
+			MaxTorqueYaw_High = 25 * 2,
+
 			MAX_SPEED = LowGearSpeed,
 			ACCEL = 10*2,    -- Thruster Acceleration m/s2 (1g = 9.8)
 			BRAKE = ACCEL, 
@@ -322,15 +339,30 @@ MainRobot = {
 				{p=25, i=0, d=5},
 				right_pid=
 				{p=25, i=0, d=5},					--These should always match, but able to be made different
-				latency=0.300,
+				--latency=0.300,
 				--I'm explicitly keeping this here to show that we have the same ratio (it is conceivable that this would not always be true)
 				--This is obtainer from encoder RPM's of 1069.2 and Wheel RPM's 427.68 (both high and low have same ratio)
-				encoder_to_wheel_ratio=0.4,			--example if encoder spins at 1069.2 multiply by this to get 427.68 (for the wheel rpm)
-				voltage_multiply=1.0,				--May be reversed using -1.0
+				encoder_to_wheel_ratio=0.5,			--example if encoder spins at 1069.2 multiply by this to get 427.68 (for the wheel rpm)
+				voltage_multiply=-1.0,				--May be reversed using -1.0
 				reverse_steering='no',
 				 left_encoder_reversed='no',
 				right_encoder_reversed='no',
-				inv_max_accel = 0.0  --solved empiracally
+				inv_max_accel = 1/30,  --solved empiracally
+				--inv_max_accel = 0,
+				motor_specs =
+				{
+					wheel_mass=1.5,
+					cof_efficiency=1.0,
+					gear_reduction=5310.0/403.92,
+					torque_on_wheel_radius=Inches2Meters * 1,
+					drive_wheel_radius=Inches2Meters * 2,
+					number_of_motors=1,
+					
+					free_speed_rpm=5310.0,
+					stall_torque=6.561,
+					stall_current_amp=399,
+					free_current_amp=8.1
+				}
 			}
 		}
 	},
@@ -416,6 +448,7 @@ MainRobot = {
 			Joystick_SetCurrentSpeed_2 = {type="joystick_analog", key=1, is_flipped=true, multiplier=1.0, filter=0.1, curve_intensity=0.0},
 			Robot_SetLowGearOff = {type="joystick_button", key=2, on_off=false},
 			Robot_SetLowGearOn = {type="joystick_button", key=1, on_off=false},
+			Winch_SetGoalShot = {type="joystick_button", key=3, on_off=false},
 						
 			POV_Turn =  {type="joystick_analog", key=8, is_flipped=false, multiplier=1.0, filter=0.0, curve_intensity=0.0},
 			Robot_SetDriverOverride = {type="joystick_button", key=5, on_off=true},
